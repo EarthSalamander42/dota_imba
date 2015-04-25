fa02b338efac236fd17d7b1c7d2ea4a4ee00f1ba
Felipe Calife <fcalife@gmail.com>
2015-04-25 03:04:24 -0300

    Progress on Jakiro: finished Dual Breath, Ice Path and Liquid Fire.

--- scripts/vscripts/abilities/hero_jakiro.lua
+++ scripts/vscripts/abilities/hero_jakiro.lua
@@ -1,24 +1,22 @@
---[[
-	Author: Ractidous
-	Date: 27.01.2015.
-	Launch the icy breath
-]]
-function Launch_IcyBreath( event )
-	local caster = event.caster
-	local ability = event.ability
+--[[	Author: Ractidous
+		Date: 27.01.2015.	]]
 
-	local casterOrigin = caster:GetAbsOrigin()
-	local targetPos = event.target_points[1]
-	local direction = targetPos - casterOrigin
-	direction = direction / direction:Length2D()
+function LaunchIcyBreath( keys )
+	local caster = keys.caster
+	local ability = keys.ability
+
+	local caster_pos = caster:GetAbsOrigin()
+	local target_pos = keys.target_points[1]
+	local direction = target_pos - caster_pos
+	direction = direction:Normalized()
 
 	ProjectileManager:CreateLinearProjectile( {
 		Ability				= ability,
 	--	EffectName			= "",
-		vSpawnOrigin		= casterOrigin,
-		fDistance			= event.distance,
-		fStartRadius		= event.start_radius,
-		fEndRadius			= event.end_radius,
+		vSpawnOrigin		= caster_pos,
+		fDistance			= keys.distance,
+		fStartRadius		= keys.start_radius,
+		fEndRadius			= keys.end_radius,
 		Source				= caster,
 		bHasFrontalCone		= true,
 		bReplaceExisting	= false,
@@ -27,45 +25,41 @@ function Launch_IcyBreath( event )
 		iUnitTargetType		= DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_CREEP + DOTA_UNIT_TARGET_MECHANICAL,
 	--	fExpireTime			= ,
 		bDeleteOnHit		= false,
-		vVelocity			= direction * event.speed,
+		vVelocity			= direction * keys.speed,
 		bProvidesVision		= false,
 	--	iVisionRadius		= ,
 	--	iVisionTeamNumber	= caster:GetTeamNumber(),
 	} )
 
-	local particleName = "particles/units/heroes/hero_jakiro/jakiro_dual_breath_ice.vpcf"
-	local pfx = ParticleManager:CreateParticle( particleName, PATTACH_ABSORIGIN, caster )
-	ParticleManager:SetParticleControl( pfx, 0, casterOrigin )
-	ParticleManager:SetParticleControl( pfx, 1, direction * event.speed * 1.333 )
+	local particle_name = keys.particle_name
+	local pfx = ParticleManager:CreateParticle( particle_name, PATTACH_ABSORIGIN, caster )
+	ParticleManager:SetParticleControl( pfx, 0, caster_pos )
+	ParticleManager:SetParticleControl( pfx, 1, direction * keys.speed * 1.333 )
 	ParticleManager:SetParticleControl( pfx, 3, Vector(0,0,0) )
-	ParticleManager:SetParticleControl( pfx, 9, casterOrigin )
+	ParticleManager:SetParticleControl( pfx, 9, caster_pos )
 
 	caster:SetContextThink( DoUniqueString( "destroy_particle" ), function ()
 		ParticleManager:DestroyParticle( pfx, false )
-	end, event.distance / event.speed )
+	end, keys.distance / keys.speed )
+	
 end
 
---[[
-	Author: Ractidous
-	Date: 27.01.2015.
-	Launch the fiery breath
-]]
-function Launch_FieryBreath( event )
-	local caster = event.caster
-	local fieryAbility = caster:FindAbilityByName( event.fiery_ability_name )
+function LaunchFieryBreath( keys )
+	local caster = keys.caster
+	local fiery_ability = caster:FindAbilityByName( keys.fiery_ability_name )
 
-	local casterOrigin = caster:GetAbsOrigin()
-	local targetPos = event.target_points[1]
-	local direction = targetPos - casterOrigin
-	direction = direction / direction:Length2D()
+	local caster_pos = caster:GetAbsOrigin()
+	local target_pos = keys.target_points[1]
+	local direction = target_pos - caster_pos
+	direction = direction:Normalized()
 
 	ProjectileManager:CreateLinearProjectile( {
-		Ability				= fieryAbility,
+		Ability				= fiery_ability,
 	--	EffectName			= "",
-		vSpawnOrigin		= casterOrigin,
-		fDistance			= event.distance,
-		fStartRadius		= event.start_radius,
-		fEndRadius			= event.end_radius,
+		vSpawnOrigin		= caster_pos,
+		fDistance			= keys.distance,
+		fStartRadius		= keys.start_radius,
+		fEndRadius			= keys.end_radius,
 		Source				= caster,
 		bHasFrontalCone		= true,
 		bReplaceExisting	= false,
@@ -74,215 +68,65 @@ function Launch_FieryBreath( event )
 		iUnitTargetType		= DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_CREEP + DOTA_UNIT_TARGET_MECHANICAL,
 	--	fExpireTime			= ,
 		bDeleteOnHit		= false,
-		vVelocity			= direction * event.speed,
+		vVelocity			= direction * keys.speed,
 		bProvidesVision		= false,
 	--	iVisionRadius		= ,
 	--	iVisionTeamNumber	= caster:GetTeamNumber(),
 	} )
 
-	local particleName = "particles/units/heroes/hero_jakiro/jakiro_dual_breath_fire.vpcf"
-	local pfx = ParticleManager:CreateParticle( particleName, PATTACH_ABSORIGIN, caster )
-	ParticleManager:SetParticleControl( pfx, 0, casterOrigin )
-	ParticleManager:SetParticleControl( pfx, 1, direction * event.speed * 1.333 )
+	local particle_name = keys.particle_name
+	local pfx = ParticleManager:CreateParticle( particle_name, PATTACH_ABSORIGIN, caster )
+	ParticleManager:SetParticleControl( pfx, 0, caster_pos )
+	ParticleManager:SetParticleControl( pfx, 1, direction * keys.speed * 1.333 )
 	ParticleManager:SetParticleControl( pfx, 3, Vector(0,0,0) )
-	ParticleManager:SetParticleControl( pfx, 9, casterOrigin )
+	ParticleManager:SetParticleControl( pfx, 9, caster_pos )
 
 	caster:SetContextThink( DoUniqueString( "destroy_particle" ), function ()
 		ParticleManager:DestroyParticle( pfx, false )
-	end, event.distance / event.speed )
+	end, keys.distance / keys.speed )
 end
 
---[[
-	Author: Ractidous
-	Date: 26.01.2015.
-	Apply burn modifier to the target.
-]]
-function OnProjectileHit_Fiery( event )
-	local caster = event.caster
-	local target = event.target
-	local ability = caster:FindAbilityByName( event.main_ability_name )
+function DualBreathInitialize( keys )
+	local caster = keys.caster
+	local ability = keys.ability
+	local target = keys.target
+	local modifier = keys.modifier
+	local duration = ability:GetLevelSpecialValueFor("tooltip_duration", ability:GetLevel() - 1)
 
-	ability:ApplyDataDrivenModifier( caster, target, event.modifier_name, {} )
+	-- Resets the debuff's duration when a target is affected
+	ability:ApplyDataDrivenModifier(caster, target, modifier, {duration = duration})
+	target.dual_breath_duration = duration
 end
 
---[[
-	Author: Ractidous
-	Date: 26.01.2015.
-	Create the particle effect.
-]]
-function FireEffect_IcePath( event )
-	local caster		= event.caster
-	local ability		= event.ability
-	local pathLength	= ability:GetCastRange()
-	local pathDelay		= event.path_delay
-	local pathDuration	= event.duration
-	local pathRadius	= event.path_radius
+function DualBreathCountdown( keys )
+	local caster = keys.caster
+	local ability = keys.ability
+	local target = keys.target
+	local damage_interval = ability:GetLevelSpecialValueFor("damage_interval", ability:GetLevel() - 1)
 
-	local startPos = caster:GetAbsOrigin()
-	local endPos = startPos + caster:GetForwardVector() * pathLength
+	local modifier = keys.modifier
+	local modifier_macropyre = keys.modifier_macropyre
 
-	ability.ice_path_stunStart	= GameRules:GetGameTime() + pathDelay
-	ability.ice_path_stunEnd	= GameRules:GetGameTime() + pathDelay + pathDuration
-
-	ability.ice_path_startPos	= startPos * 1
-	ability.ice_path_endPos		= endPos * 1
-
-	ability.ice_path_startPos.z = 0
-	ability.ice_path_endPos.z = 0
-
-	-- Create ice_path
-	local particleName = "particles/units/heroes/hero_jakiro/jakiro_ice_path.vpcf"
-	local pfx = ParticleManager:CreateParticle( particleName, PATTACH_ABSORIGIN, caster )
-	ParticleManager:SetParticleControl( pfx, 0, startPos )
-	ParticleManager:SetParticleControl( pfx, 1, endPos )
-	ParticleManager:SetParticleControl( pfx, 2, startPos )
-
-	ability.pfxIcePath = pfx
-
-	-- Create ice_path_b
-	particleName = "particles/units/heroes/hero_jakiro/jakiro_ice_path_b.vpcf"
-	pfx = ParticleManager:CreateParticle( particleName, PATTACH_ABSORIGIN, caster )
-	ParticleManager:SetParticleControl( pfx, 0, startPos )
-	ParticleManager:SetParticleControl( pfx, 1, endPos )
-	ParticleManager:SetParticleControl( pfx, 2, Vector( pathDelay + pathDuration, 0, 0 ) )
-	ParticleManager:SetParticleControl( pfx, 9, startPos )
-
-	-- Generate projectiles
-	if pathRadius < 32 then
-		print( "Set the proper value of path_radius in ice_path_datadriven." )
-		return
-	end
-
-	local projectileRadius = pathRadius * math.sqrt(2)
-	local numProjectiles = math.floor( pathLength / (pathRadius*2) ) + 1
-	local stepLength = pathLength / ( numProjectiles - 1 )
-
-	for i=1, numProjectiles do
-		local projectilePos = startPos + caster:GetForwardVector() * (i-1) * stepLength
-
-		ProjectileManager:CreateLinearProjectile( {
-			Ability				= ability,
-		--	EffectName			= "",
-			vSpawnOrigin		= projectilePos,
-			fDistance			= 64,
-			fStartRadius		= projectileRadius,
-			fEndRadius			= projectileRadius,
-			Source				= caster,
-			bHasFrontalCone		= false,
-			bReplaceExisting	= false,
-			iUnitTargetTeam		= DOTA_UNIT_TARGET_TEAM_ENEMY,
-			iUnitTargetFlags	= DOTA_UNIT_TARGET_FLAG_NONE,
-			iUnitTargetType		= DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_CREEP + DOTA_UNIT_TARGET_MECHANICAL,
-			fExpireTime			= ability.ice_path_stunEnd,
-			bDeleteOnHit		= false,
-			vVelocity			= Vector( 0, 0, 0 ),	-- Don't move!
-			bProvidesVision		= true,
-			iVisionRadius		= 150,
-			iVisionTeamNumber	= caster:GetTeamNumber(),
-		} )
-	end
-end
-
---[[
-	Author: Ractidous
-	Date: 27.01.2015.
-	Destroy ice_path manually in order to show its endcap effects.
-]]
-function FireEffect_Destroy_IcePath_A( event )
-	local caster		= event.caster
-	local ability		= event.ability
-	local pfxIcePath	= ability.pfxIcePath
-
-	ParticleManager:DestroyParticle( pfxIcePath, false )
-
-	ability.pfxIcePath = nil
-end
-
---[[
-	Author: Ractidous
-	Data: 27.01.2015.
-	Apply a dummy modifier that periodcally checks whether the target is within the ice path.
-]]
-function ApplyDummyModifier( event )
-	local caster = event.caster
-	local target = event.target
-	local ability = event.ability
-	local modifierName = event.modifier_name
-
-	local duration = ability.ice_path_stunEnd - GameRules:GetGameTime()
-
-	ability:ApplyDataDrivenModifier( caster, target, modifierName, { duration = duration } )
-end
-
---[[
-	Author: Ractidous
-	Date: 27.01.2015.
-	Check whether the target is within the ice path, and apply stun and damage if neccesary.
-]]
-function CheckIcePath( event )
-	local caster		= event.caster
-	local target		= event.target
-	local ability		= event.ability
-	local pathRadius	= event.path_radius
-
-	local stunModifierName	= "modifier_ice_path_stun_datadriven"
-
-	if GameRules:GetGameTime() < ability.ice_path_stunStart then
-		-- Not yet.
-		return
-	end
-
-	if target:HasModifier( stunModifierName ) then
-		-- Already stunned.
-		return
-	end
-
-	local targetPos = target:GetAbsOrigin()
-	targetPos.z = 0
-
-	local distance = DistancePointSegment( targetPos, ability.ice_path_startPos, ability.ice_path_endPos )
-	if distance < pathRadius then
-		local duration = ability.ice_path_stunEnd - GameRules:GetGameTime()
-		ability:ApplyDataDrivenModifier( caster, target, stunModifierName, { duration = duration } )
-	end
-end
-
---[[
-	Author: Ractidous
-	Date: 27.01.2015.
-	Distance between a point and a segment.
-]]
-function DistancePointSegment( p, v, w )
-	local l = w - v
-	local l2 = l:Dot( l )
-	t = ( p - v ):Dot( w - v ) / l2
-	if t < 0.0 then
-		return ( v - p ):Length2D()
-	elseif t > 1.0 then
-		return ( w - p ):Length2D()
+	-- Checks if the target is in Macropyre's area. If yes, renews the dual breath debuff.
+	if target:HasModifier(modifier_macropyre) then
+		ability:ApplyDataDrivenModifier(caster, target, modifier, {duration = target.dual_breath_duration})
 	else
-		local proj = v + t * l
-		return ( proj - p ):Length2D()
+		target.dual_breath_duration = target.dual_breath_duration - damage_interval
 	end
 end
 
---[[
-	Author: Ractidous
-	Date: 28.01.2015.
-	Start cooldown.
-]]
-function StartCooldown( event )
-	local caster = event.caster
-	local ability = event.ability
+function LiquidFireCooldown( keys )
+	local caster = keys.caster
+	local ability = keys.ability
 	local cooldown = ability:GetCooldown( ability:GetLevel() - 1 )
-	local modifierName = "modifier_liquid_fire_orb_datadriven"
+	local modifier = "modifier_imba_liquid_fire_orb"
 
 	-- Start cooldown
 	ability:EndCooldown()
 	ability:StartCooldown( cooldown )
 
 	-- Disable orb modifier
-	caster:RemoveModifierByName( "modifier_liquid_fire_orb_datadriven" )
+	caster:RemoveModifierByName( modifier )
 
 	-- Re-enable orb modifier after for the duration
 	ability:SetContextThink( DoUniqueString("activateLiquidFire"), function ()
@@ -293,14 +137,9 @@ function StartCooldown( event )
 	end, cooldown + 0.05 )
 end
 
---[[
-	Author: Ractidous
-	Dage: 28.01.2015.
-	Check orb modifer state on upgrading.
-]]
-function CheckOrbModifier( event )
-	local ability = event.ability
-	local caster = event.caster
+function LiquidFireCheckOrb( keys )
+	local ability = keys.ability
+	local caster = keys.caster
 
 	if ability.liquid_fire_forceEnableOrb then
 		ability.liquid_fire_forceEnableOrb = nil
@@ -311,7 +150,36 @@ function CheckOrbModifier( event )
 		return
 	end
 
-	caster:RemoveModifierByName( "modifier_liquid_fire_orb_datadriven" )
+	caster:RemoveModifierByName( "modifier_imba_liquid_fire_orb" )
+end
+
+function LiquidFireInitialize( keys )
+	local caster = keys.caster
+	local ability = keys.ability
+	local target = keys.target
+	local modifier = keys.modifier
+	local duration = ability:GetLevelSpecialValueFor("duration", ability:GetLevel() - 1)
+
+	-- Resets the debuff's duration when a target is affected
+	ability:ApplyDataDrivenModifier(caster, target, modifier, {duration = duration})
+	target.liquid_fire_duration = duration
+end
+
+function LiquidFireCountdown( keys )
+	local caster = keys.caster
+	local ability = keys.ability
+	local target = keys.target
+	local damage_interval = ability:GetLevelSpecialValueFor("damage_interval", ability:GetLevel() - 1)
+
+	local modifier = keys.modifier
+	local modifier_macropyre = keys.modifier_macropyre
+
+	-- Checks if the target is in Macropyre's area. If yes, renews the liquid fire debuff.
+	if target:HasModifier(modifier_macropyre) then
+		ability:ApplyDataDrivenModifier(caster, target, modifier, {duration = target.liquid_fire_duration})
+	else
+		target.liquid_fire_duration = target.liquid_fire_duration - damage_interval
+	end
 end
 
 --[[
@@ -319,13 +187,13 @@ end
 	Date: 27.01.2015.
 	Create the particle effect and projectiles.
 ]]
-function FireMacropyre( event )
-	local caster		= event.caster
-	local ability		= event.ability
+function FireMacropyre( keys )
+	local caster		= keys.caster
+	local ability		= keys.ability
 
-	local pathLength	= event.cast_range
-	local pathRadius	= event.path_radius
-	local duration		= event.duration
+	local pathLength	= keys.cast_range
+	local pathRadius	= keys.path_radius
+	local duration		= keys.duration
 
 	local startPos = caster:GetAbsOrigin()
 	local endPos = startPos + caster:GetForwardVector() * pathLength
@@ -335,8 +203,8 @@ function FireMacropyre( event )
 	ability.macropyre_expireTime = GameRules:GetGameTime() + duration
 
 	-- Create particle effect
-	local particleName = "particles/units/heroes/hero_jakiro/jakiro_macropyre.vpcf"
-	local pfx = ParticleManager:CreateParticle( particleName, PATTACH_ABSORIGIN, caster )
+	local particle_name = "particles/units/heroes/hero_jakiro/jakiro_macropyre.vpcf"
+	local pfx = ParticleManager:CreateParticle( particle_name, PATTACH_ABSORIGIN, caster )
 	ParticleManager:SetParticleControl( pfx, 0, startPos )
 	ParticleManager:SetParticleControl( pfx, 1, endPos )
 	ParticleManager:SetParticleControl( pfx, 2, Vector( duration, 0, 0 ) )
@@ -387,11 +255,11 @@ end
 	Data: 27.01.2015.
 	Apply a dummy modifier that periodcally checks whether the target is within the macropyre's path.
 ]]
-function ApplyDummyModifier( event )
-	local caster = event.caster
-	local target = event.target
-	local ability = event.ability
-	local modifierName = event.modifier_name
+function ApplyDummyModifier( keys )
+	local caster = keys.caster
+	local target = keys.target
+	local ability = keys.ability
+	local modifierName = keys.modifier_name
 
 	local duration = ability.macropyre_expireTime - GameRules:GetGameTime()
 
@@ -403,17 +271,17 @@ end
 	Date: 27.01.2015.
 	Check whether the target is within the path, and apply damage if neccesary.
 ]]
-function CheckMacropyre( event )
-	local caster		= event.caster
-	local target		= event.target
-	local ability		= event.ability
-	local pathRadius	= event.path_radius
-	local damage		= event.damage
+function CheckMacropyre( keys )
+	local caster		= keys.caster
+	local target		= keys.target
+	local ability		= keys.ability
+	local pathRadius	= keys.path_radius
+	local damage		= keys.damage
 
-	local targetPos = target:GetAbsOrigin()
-	targetPos.z = 0
+	local target_pos = target:GetAbsOrigin()
+	target_pos.z = 0
 
-	local distance = DistancePointSegment( targetPos, ability.macropyre_startPos, ability.macropyre_endPos )
+	local distance = DistancePointSegment( target_pos, ability.macropyre_startPos, ability.macropyre_endPos )
 	if distance < pathRadius then
 		-- Apply damage
 		ApplyDamage( {
