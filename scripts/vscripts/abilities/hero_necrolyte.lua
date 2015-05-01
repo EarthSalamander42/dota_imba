2888b673bece6640157f90e3ebb042d09f868a48
Felipe Calife <fcalife@gmail.com>
2015-05-01 02:38:40 -0300

    Fixed some necrolyte bugs. Still need to fix reaper's scythe's respawn time increase.

--- scripts/vscripts/abilities/hero_necrolyte.lua
+++ scripts/vscripts/abilities/hero_necrolyte.lua
@@ -5,6 +5,7 @@ function DeathPulse( keys )
 	local stack_buff = keys.stack_buff
 	local stack_debuff = keys.stack_debuff
 
+	-- Ability parameters
 	local ability_level = ability:GetLevel() - 1
 	local damage = ability:GetLevelSpecialValueFor("damage", ability_level)
 	local heal = ability:GetLevelSpecialValueFor("heal", ability_level)
@@ -41,22 +42,53 @@ function Heartstopper( keys )
 	local stack_modifier = keys.stack_modifier
 	local visibility_modifier = keys.visible_modifier
 
-	AddStacks(ability, caster, target, stack_modifier, 1, true)
+	-- Ability parameters
+	local ability_level = ability:GetLevel() - 1
+	local damage = ability:GetLevelSpecialValueFor("aura_damage", ability_level)
+	local damage_interval = ability:GetLevelSpecialValueFor("aura_damage_interval", ability_level)
+	local stack_power = ability:GetLevelSpecialValueFor("stack_power", ability_level)
 
+	-- Calculate damage
+	AddStacks(ability, caster, target, stack_modifier, 1, true)
+	local max_hp = target:GetMaxHealth()
+	local stack_count = target:GetModifierStackCount(stack_modifier, ability)
+	damage = damage * max_hp * ( 1 + stack_power * stack_count / 100 ) / 100
+	
+	-- Damage is dealt by modifying the target's HP directly (HP removal). Deals 1 pure damage to kill targets when appropriate
+	if target:GetHealth() <= damage then
+		ApplyDamage({attacker = caster, victim = target, ability = ability, damage = damage, damage_type = DAMAGE_TYPE_PURE})
+	else
+		target:SetHealth(target:GetHealth() - damage)
+	end
+
+	-- Modifier is only visible if the enemy team has vision of Necrophos
 	if target:CanEntityBeSeenByMyTeam(caster) then
 		if not target:HasModifier(visibility_modifier) then
 			ability:ApplyDataDrivenModifier(caster, target, visibility_modifier, {})
 		end
-		target:SetModifierStackCount(visibility_modifier, ability, target:GetModifierStackCount(stack_modifier, ability))
+		target:SetModifierStackCount(visibility_modifier, ability, target:GetModifierStackCount(stack_modifier, ability) )
 	else
 		target:RemoveModifierByName(visibility_modifier)
 	end
 end
 
-function Sadist( keys )
+function HeartstopperEnd( keys )
 	local caster = keys.caster
 	local ability = keys.ability
 	local target = keys.target
+	local stack_modifier = keys.stack_modifier
+	local visibility_modifier = keys.visible_modifier
+
+	local stack_count = target:GetModifierStackCount(stack_modifier, ability)
+	RemoveStacks(ability, target, stack_modifier, stack_count)
+	stack_count = target:GetModifierStackCount(visibility_modifier, ability)
+	RemoveStacks(ability, target, visibility_modifier, stack_count)
+end
+
+function Sadist( keys )
+	local caster = keys.caster
+	local ability = keys.ability
+	local target = keys.unit
 	local regen_modifier = keys.regen_modifier
 
 	local hero_multiplier = ability:GetLevelSpecialValueFor("hero_multiplier", ability:GetLevel() - 1 )
@@ -107,7 +139,7 @@ function ReapersScythe( keys )
 		-- Calculates and deals damage
 		local damage_bonus = 1 - target:GetHealth() / target:GetMaxHealth() 
 		damage = damage * target:GetMaxHealth() * (1 + damage_bonus) / 100
-		ApplyDamage(attacker = caster, victim = target, ability = ability, damage = damage, damage_type = DAMAGE_TYPE_PURE)
+		ApplyDamage({attacker = caster, victim = target, ability = ability, damage = damage, damage_type = DAMAGE_TYPE_PURE})
 
 		-- Initializes the buyback variable if necessary
 		if not target.scythe_added_respawn then
