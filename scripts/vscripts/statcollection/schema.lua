5d0003ac38bafd21c03582a1df0ccb109af0405b
jimmydorry <jimmydorry@users.noreply.github.com>
2015-10-26 14:15:42 +1000

    Update schema.lua

--- scripts/vscripts/statcollection/schema.lua
+++ scripts/vscripts/statcollection/schema.lua
@@ -1,7 +1,6 @@
 customSchema = class({})
 
 function customSchema:init()
-
 	-- Check the schema_examples folder for different implementations
 
 	-- Tracks game version
@@ -10,57 +9,37 @@ function customSchema:init()
 	
 	-- Listen for changes in the current state
 	ListenToGameEvent('game_rules_state_change', function(keys)
-		local state = GameRules:State_Get()
+        local state = GameRules:State_Get()
 
-		-- Send custom stats when the game ends
-		if state == DOTA_GAMERULES_STATE_POST_GAME then
-			print("got to post game stage")
+        -- Send custom stats when the game ends
+        if state == DOTA_GAMERULES_STATE_POST_GAME then
 
-			-- Build game array
-			local game = BuildGameArray()
-			print("built game array")
+            -- Build game array
+            local game = BuildGameArray()
 
-			-- Build players array
-			local players = BuildPlayersArray()
-			print("built players array")
+            -- Build players array
+            local players = BuildPlayersArray()
 
-			-- Print the schema data to the console
-			if statCollection.TESTING then
-				PrintSchema(game,players)
-				print("printed schema")
-			end
+            -- Print the schema data to the console
+            if statCollection.TESTING then
+                PrintSchema(game,players)
+            end
 
-			-- Send custom stats
-			if statCollection.HAS_SCHEMA then
-				statCollection:sendCustom({game=game, players=players})
-				print("sent stats")
-			end
-		end
-	end, nil)
+            -- Send custom stats
+            if statCollection.HAS_SCHEMA then
+                statCollection:sendCustom({game=game, players=players})
+            end
+        end
+    end, nil)
 end
 
 -------------------------------------
 
-function customSchema:submitRound(args)
-	winners = BuildRoundWinnerArray()
-	game = BuildGameArray()
-	players = BuildPlayersArray()
-
-	statCollection:sendCustom({game=game, players=players})
-
-	return {winners = winners, lastRound = false}
-end
-
--------------------------------------
-
-function BuildRoundWinnerArray()
-	local winners = {}
-	return winners
-end
+-- In the statcollection/lib/utilities.lua, you'll find many useful functions to build your schema.
+-- You are also encouraged to call your custom mod-specific functions
 
 -- Returns a table with our custom game tracking.
 function BuildGameArray()
-
 	local game = {}
 
 	-- Tracks total game length, from the horn sound, in seconds
@@ -90,15 +69,16 @@ function BuildPlayersArray()
 				end
 
 				table.insert(players, {
-
 					-- SteamID32 required in here
 					steamID32 = PlayerResource:GetSteamAccountID(playerID),
 
-					nam = GetHeroName(playerID),	-- Hero by its short name
+					-- Example functions for generic stats are defined in statcollection/lib/utilities.lua
+                    			-- Add player values here as someValue = GetSomePlayerValue(),
+					nam = GetHeroName(playerID),		-- Hero by its short name
 					lvl = hero:GetLevel(),			-- Hero level at the end of the game
 					pnw = GetNetworth(hero),		-- Sum of hero gold and item worth
 					pbb = hero.buyback_count,		-- Amount of buybacks performed during the game
-					pt = player_team,				-- Team this hero belongs to
+					pt = player_team,			-- Team this hero belongs to
 					pk = hero:GetKills(),			-- Number of kills of this players hero
 					pa = hero:GetAssists(),			-- Number of deaths of this players hero
 					pd = hero:GetDeaths(),			-- Number of deaths of this players hero
@@ -111,67 +91,49 @@ function BuildPlayersArray()
 	return players
 end
 
--------------------------------------
---          Stat Functions         --
--------------------------------------
-
+-- Prints the custom schema, required to get an schemaID
 function PrintSchema( gameArray, playerArray )
-	print("--------- GAME DATA ---------")
-	DeepPrintTable(gameArray)
-	print("\n-------- PLAYER DATA --------")
-	DeepPrintTable(playerArray)
-	print("-----------------------------")
+    print("-------- GAME DATA --------")
+    DeepPrintTable(gameArray)
+    print("\n-------- PLAYER DATA --------")
+    DeepPrintTable(playerArray)
+    print("-------------------------------------")
 end
 
-function GetHeroName( playerID )
-	local heroName = PlayerResource:GetSelectedHeroName( playerID )
-	heroName = string.gsub(heroName,"npc_dota_hero_","")
-	return heroName
+-- Write 'test_schema' on the console to test your current functions instead of having to end the game
+if Convars:GetBool('developer') then
+    Convars:RegisterCommand("test_schema", function() PrintSchema(BuildGameArray(),BuildPlayersArray()) end, "Test the custom schema arrays", 0)
 end
 
-function GetNetworth( hero )
-	local gold = hero:GetGold()
+-------------------------------------
 
-	-- Iterate over item slots adding up its gold cost
-	for i = 0,15 do
-		local item = hero:GetItemInSlot(i)
-		if item then
-			gold = gold + item:GetCost()
-		end
-	end
+-- If your gamemode is round-based, you can use statCollection:submitRound(bLastRound) at any point of your main game logic code to send a round
+-- If you intend to send rounds, make sure your settings.kv has the 'HAS_ROUNDS' set to true. Each round will send the game and player arrays defined earlier
+-- The round number is incremented internally, lastRound can be marked to notify that the game ended properly
+function customSchema:submitRound(isLastRound)
 
-	return gold
+    local winners = BuildRoundWinnerArray()
+    local game = BuildGameArray()
+    local players = BuildPlayersArray()
+
+    statCollection:sendCustom({game=game, players=players})
+
+    isLastRound = isLastRound or false --If the function is passed with no parameter, default to false.
+    return {winners = winners, lastRound = isLastRound}
 end
 
-function GetItemName(hero, slot)
-	local item = hero:GetItemInSlot(slot)
-	if item then
-		local itemName = item:GetAbilityName()
-		itemName = string.gsub(itemName,"item_","") --Cuts the item_ prefix
-		return itemName
-	else
-		return ""
-	end
+-- A list of players marking who won this round
+function BuildRoundWinnerArray()
+    local winners = {}
+    local current_winner_team = GameRules.Winner or 0 --You'll need to provide your own way of determining which team won the round
+    for playerID = 0, DOTA_MAX_PLAYERS do
+        if PlayerResource:IsValidPlayerID(playerID) then
+            if not PlayerResource:IsBroadcaster(playerID) then
+                winners[PlayerResource:GetSteamAccountID(playerID)] = (PlayerResource:GetTeam(playerID) == current_winner_team) and 1 or 0
+            end
+        end
+    end
+    return winners
 end
 
-function GetItemList(hero)
-	local itemTable = {}
-
-	for i=0,5 do
-		local item = hero:GetItemInSlot(i)
-		if item then
-			if string.find(item:GetAbilityName(), "imba") then
-				local itemName = string.gsub(item:GetAbilityName(),"item_imba_","")
-				table.insert(itemTable,itemName)
-			else
-				local itemName = string.gsub(item:GetAbilityName(),"item_","")
-				table.insert(itemTable,itemName)
-			end
-		end
-	end
-
-	table.sort(itemTable)
-	local itemList = table.concat(itemTable, ",")
-
-	return itemList
-end
+-------------------------------------
