d42b7babed0a1c1b2cb87a45458dd6c2ef45d4f4
jimmydorry <jimmydorry@users.noreply.github.com>
2015-10-26 14:08:11 +1000

    Update statcollection.lua

--- scripts/vscripts/statcollection/lib/statcollection.lua
+++ scripts/vscripts/statcollection/lib/statcollection.lua
@@ -28,7 +28,7 @@ local statInfo = LoadKeyValues('scripts/vscripts/statcollection/settings.kv')
 local postLocation = 'http://getdotastats.com/s2/api/'
 
 -- The schema version we are currently using
-local schemaVersion = 1
+local schemaVersion = 2
 
 -- Constants used for pretty formatting, as well as strings
 local printPrefix = 'Stat Collection: '
@@ -57,6 +57,7 @@ local messagePhase1Complete = 'Match was successfully registered with GetDotaSta
 local messagePhase2Complete = 'Match pregame settings have been recorded!'
 local messagePhase3Complete = 'Match stats were successfully recorded!'
 local messageCustomComplete = 'Match custom stats were successfully recorded!'
+local messageFlagsSet       = 'Flag was successfully set!'
 
 -- Store the first detected steamID
 local firstConnectedSteamID = -1
@@ -88,15 +89,8 @@ function statCollection:init()
     -- Print the intro message
     print(printPrefix .. messageStarting)
 
-    -- Load up the settings
-    local modIdentifier = statInfo.modID
-    local schemaID = statInfo.schemaID
-    local HAS_SCHEMA = statInfo.schemaID ~= 'XXXXXXXXXXXXXXXX'
-    local HAS_ROUNDS = statInfo.HAS_ROUNDS
-    local GAME_WINNER = statInfo.GAME_WINNER
-    local ANCIENT_EXPLOSION = statInfo.ANCIENT_EXPLOSION
-
     -- Check for a modIdentifier
+    local modIdentifier = statInfo.modID
     if not modIdentifier then
         print(printPrefix .. errorMissingModIdentifier)
 
@@ -106,12 +100,6 @@ function statCollection:init()
         self.doneInit = false
         return
     end
-
-    -- Set settings
-    self.HAS_SCHEMA = HAS_SCHEMA
-    self.HAS_ROUNDS = tobool(statInfo.HAS_ROUNDS)
-    self.GAME_WINNER = tobool(statInfo.GAME_WINNER)
-    self.ANCIENT_EXPLOSION = tobool(statInfo.ANCIENT_EXPLOSION)
 
     --[[ Check for a schemaIdentifier
     if not schemaID then
@@ -123,14 +111,18 @@ function statCollection:init()
         return
     end]]
 
+    -- Load and set settings
+    self.HAS_SCHEMA = statInfo.schemaID ~= 'XXXXXXXXXXXXXXXX'
+    self.HAS_ROUNDS = tobool(statInfo.HAS_ROUNDS)
+    self.GAME_WINNER = tobool(statInfo.GAME_WINNER)
+    self.ANCIENT_EXPLOSION = tobool(statInfo.ANCIENT_EXPLOSION)
+    self.TESTING = tobool(statInfo.TESTING)
+
     -- Store the modIdentifier
     self.modIdentifier = modIdentifier
 
     -- Store the schemaIdentifier
     self.SCHEMA_KEY = statInfo.schemaID
-
-    -- Reset our flags store
-    self.flags = {}
 
     -- Set the default winner to -1 (no winner)
     self.winner = -1
@@ -236,6 +228,7 @@ function statCollection:setFlags(flags)
         -- Store the new flags
         for flagKey,flagValue in pairs(flags) do
             self.flags[flagKey] = flagValue
+            print(printPrefix .. messageFlagsSet .. " {"..flagKey..":"..tostring(flagValue).."}")
         end
         
     else
@@ -334,12 +327,14 @@ function statCollection:sendStage2()
 
     -- Build players array
     local players = {}
-    for i = 1, (PlayerResource:GetPlayerCount() or 1) do
-        table.insert(players, {
-            playerName = PlayerResource:GetPlayerName(i - 1),
-            steamID32 = PlayerResource:GetSteamAccountID(i - 1),
-            connectionState = PlayerResource:GetConnectionState(i - 1)
-        })
+    for playerID = 0, DOTA_MAX_PLAYERS do
+        if PlayerResource:IsValidPlayerID(playerID) then
+            table.insert(players, {
+                playerName = PlayerResource:GetPlayerName(playerID),
+                steamID32 = PlayerResource:GetSteamAccountID(playerID),
+                connectionState = PlayerResource:GetConnectionState(playerID)
+            })
+        end
     end
 
     local payload = {
@@ -478,7 +473,6 @@ function statCollection:sendCustom(args)
     -- Print the intro message
     print(printPrefix .. messageCustomStarting)
 
-    -- Build rounds table
     -- Build rounds table
     rounds = {}
     rounds[tostring(self.roundID)] = {
