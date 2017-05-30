19a3585666d3924dbd90a76a96382b30377060c3
Wouter <wouter.zegers@gmail.com>
2017-05-30 10:20:48 +0200

    Added particles for projectile and root

--- game/dota_addons/dota_imba/scripts/vscripts/hero/hero_pudge.lua
+++ game/dota_addons/dota_imba/scripts/vscripts/hero/hero_pudge.lua
@@ -371,7 +371,7 @@ function imba_pudge_meat_hook:OnSpellStart()
 			iUnitTargetTeam = DOTA_UNIT_TARGET_TEAM_BOTH,
 			iUnitTargetType = DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_BASIC,
 			iUnitTargetFlags = DOTA_UNIT_TARGET_FLAG_MAGIC_IMMUNE_ENEMIES + DOTA_UNIT_TARGET_FLAG_NOT_ANCIENTS + DOTA_UNIT_TARGET_FLAG_INVULNERABLE,
-			EffectName = "particles/hero/pudge/pudge_butchers_cleaver.vpcf",
+			--	EffectName = "particles/hero/pudge/pudge_butchers_cleaver.vpcf",
 			ExtraData = 
 			{
 				nProjectileNumber = i
@@ -429,7 +429,7 @@ function imba_pudge_meat_hook:OnProjectileHit_ExtraData( hTarget, vLocation,keys
 					FindClearSpaceForUnit( v, self.hooks[i]["vStartPosition"], false )
 				end
 			end
-			ResolveNPCPositions(vLocation, 20)
+			ResolveNPCPositions(self:GetCaster():GetAbsOrigin(), 75)
 		end
 		--self.hooks[i]["unitsHit"] = nil
 		ParticleManager:DestroyParticle( self.hooks[i]["nChainParticleFXIndex"], true )
@@ -673,8 +673,6 @@ function imba_pudge_butchers_cleaver:OnSpellStart()
 	local vTargetPosition = self:GetCursorPosition()
 	local caster = self:GetCaster()
 
-	self.modelOffset = Vector(20,-140,120)
-
 	-- I want to remove the thing but I can't find it.
 	if self:GetCaster() and self:GetCaster():IsHero() then
 		local cleaver = self:GetCaster():GetTogglableWearable( DOTA_LOADOUT_TYPE_OFFHAND_WEAPON )
@@ -695,11 +693,11 @@ function imba_pudge_butchers_cleaver:OnSpellStart()
 		iUnitTargetTeam = DOTA_UNIT_TARGET_TEAM_ENEMY,
 		iUnitTargetType = DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_BASIC,
 		iUnitTargetFlags = DOTA_UNIT_TARGET_FLAG_MAGIC_IMMUNE_ENEMIES,
-		--EffectName = "particles/hero/pudge/butchers_cleave_projectile.vpcf",
-		EffectName = "particles/units/heroes/hero_mirana/mirana_spell_arrow.vpcf",
+		EffectName = "particles/hero/pudge/butchers_cleave_projectile.vpcf",
+		--EffectName = "particles/units/heroes/hero_mirana/mirana_spell_arrow.vpcf",
 		
 	}
-
+	
 	self.projectile = ProjectileManager:CreateLinearProjectile(info)
 
 end
@@ -707,17 +705,22 @@ end
 function imba_pudge_butchers_cleaver:OnProjectileHit(hTarget,vLocation)
 	if not hTarget then 
 		for i =0,30 do
-				local cleaver = self:GetCaster():GetTogglableWearable( i )
-				if cleaver ~= nil then
-					cleaver:RemoveEffects(EF_NODRAW)
-				end
+			local cleaver = self:GetCaster():GetTogglableWearable( i )
+			if cleaver ~= nil then
+				cleaver:RemoveEffects(EF_NODRAW)		
 			end
+		end
+		UTIL_Remove(self.cleaver)
 		return 
 	end
 	
+	local nFXIndex = ParticleManager:CreateParticle( "particles/units/heroes/hero_pudge/pudge_meathook_impact.vpcf", PATTACH_CUSTOMORIGIN, hTarget )
+	ParticleManager:SetParticleControlEnt( nFXIndex, 0, hTarget, PATTACH_POINT_FOLLOW, "attach_hitloc", self:GetCaster():GetAbsOrigin(), true )
+	ParticleManager:ReleaseParticleIndex( nFXIndex )
+	
 	self.cleaver = CreateUnitByName("npc_dummy_blank",self:GetCaster():GetAbsOrigin(),false,nil,nil,self:GetCaster():GetTeamNumber())
 	self.cleaver:AddNewModifier(self:GetCaster(),self,"modifier_butchers_cleaver_dummy",{duration = self:GetSpecialValueFor("root_duration")})
-	self.cleaver.parentUnit = hTarget
+	self.cleaver.parentUnit = vHookTarget
 
 	hTarget:AddNewModifier(self:GetCaster(),self,"modifier_butchers_cleaver",{duration = self:GetSpecialValueFor("root_duration")})
 
@@ -731,8 +734,9 @@ function imba_pudge_butchers_cleaver:OnProjectileHit(hTarget,vLocation)
 	}
 	ApplyDamage( damage )
 
-	ProjectileManager:DestroyLinearProjectile(self.projectile)
+	return true
 end
+
 
 -------------------------------------------
 modifier_butchers_cleaver_dummy = class({})
@@ -758,25 +762,9 @@ function modifier_butchers_cleaver_dummy:CheckState()
 	return state
 end
 
-function modifier_butchers_cleaver_dummy:DeclareFunctions()
-	local decFuns =
-	{
-		MODIFIER_PROPERTY_MODEL_CHANGE,
-	}
-	return decFuns
-end
-
-function modifier_butchers_cleaver_dummy:GetModifierModelChange() -- This doesn't work except for the hook
-	--local model =self:GetCaster():GetTogglableWearable( DOTA_LOADOUT_TYPE_OFFHAND_WEAPON )
-	--if not model then
-		return "models/heroes/pudge/leftweapon.vmdl"
-	--end
-	--return model
-end
 function modifier_butchers_cleaver_dummy:OnCreated()
 	if IsServer() then
 		self:StartIntervalThink(FrameTime())
-		self:GetParent():SetModel("models/heroes/pudge/leftweapon.vmdl")
 	end
 end
 
@@ -794,11 +782,11 @@ function modifier_butchers_cleaver_dummy:OnDestroy()
 end
 
 function modifier_butchers_cleaver_dummy:OnIntervalThink()
+	if not self.hit then return end
 	if not self:GetParent().parentUnit or not IsValidEntity(self:GetParent().parentUnit) then
 		UTIL_Remove(self:GetParent())
 		return 
 	end
-	self:GetParent():SetForwardVector(Vector(1,0,0))
 	self:GetParent():SetAbsOrigin(self:GetParent().parentUnit:GetAbsOrigin()+self:GetAbility().modelOffset)
 
 	if (self:GetParent().parentUnit:GetAbsOrigin() -self:GetCaster():GetAbsOrigin()):Length2D() <= self:GetAbility():GetSpecialValueFor("check_radius") then
@@ -847,11 +835,11 @@ function modifier_butchers_cleaver:OnTakeDamage(keys)
 end
 
 function modifier_butchers_cleaver:GetEffectName()
-	return ""
+	return "particles/hero/pudge/pudge_cleaver_overhead.vpcf"
 end
 
 function modifier_butchers_cleaver:GetEffectAttachType()
-	return PATTACH_ABSORIGIN_FOLLOW
+	return PATTACH_OVERHEAD_FOLLOW
 end
 
 imba_pudge_dismember= class({})
@@ -973,7 +961,7 @@ end
 function modifier_dismember:OnCreated( kv )
 	self.dismember_damage = self:GetAbility():GetSpecialValueFor( "dismember_damage" )
 	self.tick_rate = self:GetAbility():GetSpecialValueFor( "tick_rate" )
-	self.strength_damage = self:GetAbility():GetSpecialValueFor( "strength_damage" )
+	self.strength_damage = self:GetAbility():GetSpecialValueFor( "strength_damage" )/100
 
 	if IsServer() then
 		self:GetParent():InterruptChannel()
