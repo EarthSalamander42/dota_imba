640383feff093294a40977a54720a417fcdc2886
X-TheDark <ventigors2008@gmail.com>
2017-06-02 15:17:51 +0300

    Changes spellfencer mechanics

--- game/dota_addons/dota_imba/scripts/vscripts/items/item_spell_fencer.lua
+++ game/dota_addons/dota_imba/scripts/vscripts/items/item_spell_fencer.lua
@@ -9,12 +9,35 @@
 
 if item_imba_spell_fencer == nil then item_imba_spell_fencer = class({}) end
 LinkLuaModifier( "modifier_item_imba_spell_fencer", "items/item_spell_fencer.lua", LUA_MODIFIER_MOTION_NONE )			-- Owner's bonus attributes, stackable
-LinkLuaModifier( "modifier_item_imba_spell_fencer_unique", "items/item_spell_fencer.lua", LUA_MODIFIER_MOTION_NONE )	-- Unique passive modifier
+LinkLuaModifier( "modifier_item_imba_spell_fencer_unique", "items/item_spell_fencer.lua", LUA_MODIFIER_MOTION_NONE )	-- Unique toggle modifier
+LinkLuaModifier( "modifier_item_imba_spell_fencer_passive_silence", "items/item_spell_fencer.lua", LUA_MODIFIER_MOTION_NONE )	-- Passive silence
 LinkLuaModifier( "modifier_item_imba_spell_fencer_buff", "items/item_spell_fencer.lua", LUA_MODIFIER_MOTION_NONE )		-- Physical damage prevention modifier
+LinkLuaModifier( "modifier_item_imba_spell_fencer_cooldown", "items/item_spell_fencer.lua", LUA_MODIFIER_MOTION_NONE )  -- Passive silence cooldown modifier
+
+function item_imba_spell_fencer:GetBehavior()
+	return DOTA_ABILITY_BEHAVIOR_IMMEDIATE + DOTA_ABILITY_BEHAVIOR_NO_TARGET + DOTA_ABILITY_BEHAVIOR_IGNORE_CHANNEL + DOTA_ABILITY_BEHAVIOR_ITEM
+end
 
 function item_imba_spell_fencer:GetIntrinsicModifierName()
 	return "modifier_item_imba_spell_fencer" end
 
+function item_imba_spell_fencer:OnSpellStart()
+	if IsServer() then
+		if self:GetCaster():HasModifier("modifier_item_imba_spell_fencer_unique") then
+			self:GetCaster():RemoveModifierByName("modifier_item_imba_spell_fencer_unique")
+		else
+			self:GetCaster():AddNewModifier(self:GetCaster(), self, "modifier_item_imba_spell_fencer_unique", {})
+		end
+	end
+end
+
+function item_imba_spell_fencer:GetAbilityTextureName()
+	if self:GetCaster():HasModifier("modifier_item_imba_spell_fencer_unique") then
+		return "custom/imba_spell_fencer"
+	end
+
+	return "custom/imba_spell_fencer_off"
+end
 -----------------------------------------------------------------------------------------------------------
 --	Spellfencer passive modifier (stackable)
 -----------------------------------------------------------------------------------------------------------
@@ -30,8 +53,8 @@ function modifier_item_imba_spell_fencer:GetAttributes() return MODIFIER_ATTRIBU
 function modifier_item_imba_spell_fencer:OnCreated(keys)
 	if IsServer() then
 		local parent = self:GetParent()
-		if not parent:HasModifier("modifier_item_imba_spell_fencer_unique") then
-			parent:AddNewModifier(parent, self:GetAbility(), "modifier_item_imba_spell_fencer_unique", {})
+		if not parent:HasModifier("modifier_item_imba_spell_fencer_passive_silence") then
+			parent:AddNewModifier(parent, self:GetAbility(), "modifier_item_imba_spell_fencer_passive_silence", {})
 		end
 	end
 end
@@ -40,8 +63,8 @@ end
 function modifier_item_imba_spell_fencer:OnDestroy(keys)
 	if IsServer() then
 		local parent = self:GetParent()
-		if not parent:HasModifier("modifier_item_imba_spell_fencer") then
-			parent:RemoveModifierByName("modifier_item_imba_spell_fencer_unique")
+		if parent:HasModifier("modifier_item_imba_spell_fencer_passive_silence") then
+			parent:RemoveModifierByName("modifier_item_imba_spell_fencer_passive_silence")
 		end
 	end
 end
@@ -73,19 +96,23 @@ function modifier_item_imba_spell_fencer:GetModifierPercentageManaRegen()
 	return self:GetAbility():GetSpecialValueFor("bonus_mana_regen") end
 
 -----------------------------------------------------------------------------------------------------------
---	Spellfencer unique modifier
+--	Spellfencer toggle damage conversion modifier
 -----------------------------------------------------------------------------------------------------------
-
 if modifier_item_imba_spell_fencer_unique == nil then modifier_item_imba_spell_fencer_unique = class({}) end
 function modifier_item_imba_spell_fencer_unique:IsHidden() return true end
 function modifier_item_imba_spell_fencer_unique:IsDebuff() return false end
 function modifier_item_imba_spell_fencer_unique:IsPurgable() return false end
 function modifier_item_imba_spell_fencer_unique:IsPermanent() return true end
 
+function modifier_item_imba_spell_fencer_unique:OnCreated( params )
+	self.damage_reduce_pct = self:GetAbility():GetSpecialValueFor("damage_reduce_pct")
+end
+
 -- Declare modifier events/properties
 function modifier_item_imba_spell_fencer_unique:DeclareFunctions()
 	local funcs = {
 		MODIFIER_EVENT_ON_ATTACK_LANDED,
+		MODIFIER_PROPERTY_DAMAGEOUTGOING_PERCENTAGE
 	}
 	return funcs
 end
@@ -94,6 +121,12 @@ end
 function modifier_item_imba_spell_fencer_unique:OnAttackLanded( keys )
 	if IsServer() then
 		local owner = self:GetParent()
+
+		-- No spellfencers in inventory
+		if ( not owner ) or ( not owner:HasModifier("modifier_item_imba_spell_fencer") ) then
+			self:Destroy()
+			return nil
+		end
 
 		-- If this attack was not performed by the modifier's owner, do nothing
 		if owner ~= keys.attacker then
@@ -113,6 +146,60 @@ function modifier_item_imba_spell_fencer_unique:OnAttackLanded( keys )
 		owner:AddNewModifier(owner, ability, "modifier_item_imba_spell_fencer_buff", {duration = 0.01})
 		target:AddNewModifier(owner, ability, "modifier_item_imba_spell_fencer_buff", {duration = 0.01})
 		ApplyDamage({attacker = owner, victim = target, ability = ability, damage = keys.original_damage, damage_type = DAMAGE_TYPE_MAGICAL, damage_flag = DOTA_DAMAGE_FLAG_NO_DAMAGE_MULTIPLIERS})
+	end
+end
+
+function modifier_item_imba_spell_fencer_unique:GetModifierDamageOutgoing_Percentage()
+	return self.damage_reduce_pct * (-1)
+end
+
+-----------------------------------------------------------------------------------------------------------
+--	Spellfencer passive magic resist debuff + on-hit silencer modifier
+-----------------------------------------------------------------------------------------------------------
+modifier_item_imba_spell_fencer_passive_silence = modifier_item_imba_spell_fencer_passive_silence or class({})
+function modifier_item_imba_spell_fencer_passive_silence:IsHidden() return true end
+function modifier_item_imba_spell_fencer_passive_silence:IsDebuff() return false end
+function modifier_item_imba_spell_fencer_passive_silence:IsPurgable() return false end
+function modifier_item_imba_spell_fencer_passive_silence:IsPermanent() return true end
+
+-- Declare modifier events/properties
+function modifier_item_imba_spell_fencer_passive_silence:DeclareFunctions()
+	local funcs = {
+		MODIFIER_EVENT_ON_ATTACK_LANDED
+	}
+	return funcs
+end
+
+function modifier_item_imba_spell_fencer_passive_silence:OnAttackLanded( keys )
+	-- If a higher-priority sword is present, do zilch
+	if IsServer() then
+		local owner = self:GetParent()
+		local ability = self:GetAbility()
+
+		-- No spellfencers in inventory
+		if ( not owner ) or ( not owner:HasModifier("modifier_item_imba_spell_fencer") ) then
+			self:Destroy()
+			return nil
+		end
+
+		-- If this attack was not performed by the modifier's owner, do nothing
+		if owner ~= keys.attacker then
+			return end
+
+		-- If this is an illusion, do nada
+		if owner:IsIllusion() then
+			return end
+
+		-- If the target is not valid, do nothing either
+		local target = keys.target
+		if (not IsHeroOrCreep(target)) then
+			return end
+
+		-- No spellfencers in inventory
+		if ( not owner ) or ( not owner:HasModifier("modifier_item_imba_spell_fencer") ) then
+			self:Destroy()
+			return nil
+		end
 
 		-- If a higher-priority sword is present, do zilch
 		local priority_sword_modifiers = {
@@ -138,12 +225,11 @@ function modifier_item_imba_spell_fencer_unique:OnAttackLanded( keys )
 		end
 
 		-- If the ability is not on cooldown, roll for a proc
-		if ability:IsCooldownReady() and RollPercentage(ability:GetSpecialValueFor("proc_chance")) then
-
+		if not owner:HasModifier("modifier_item_imba_spell_fencer_cooldown") and RollPercentage(ability:GetSpecialValueFor("proc_chance")) then
 			-- Proc! Apply the silence modifier and put the ability on cooldown
 			target:AddNewModifier(owner, ability, "modifier_item_imba_azura_silence", {duration = ability:GetSpecialValueFor("proc_duration")})
 			target:EmitSound("Imba.AzuraProc")
-			ability:UseResources(false, false, true)
+			owner:AddNewModifier(self:GetCaster(), self:GetAbility(), "modifier_item_imba_spell_fencer_cooldown", {duration = self:GetAbility():GetSpecialValueFor("proc_cooldown")})
 		end
 	end
 end
@@ -168,3 +254,16 @@ end
 
 function modifier_item_imba_spell_fencer_buff:GetAbsoluteNoDamagePhysical()
 	return 1 end
+
+-----------------------------------------------------------------------------------------------------------
+--	Spellfencer Spirit Strike internal cooldown modifier
+-----------------------------------------------------------------------------------------------------------
+modifier_item_imba_spell_fencer_cooldown = modifier_item_imba_spell_fencer_cooldown or class({})
+function modifier_item_imba_spell_fencer_cooldown:IsHidden() return false end
+function modifier_item_imba_spell_fencer_cooldown:IsDebuff() return false end
+function modifier_item_imba_spell_fencer_cooldown:IsPurgable() return false end
+function modifier_item_imba_spell_fencer_cooldown:IsPermanent() return true end
+
+function modifier_item_imba_spell_fencer_cooldown:GetTexture()
+	return "custom/imba_spell_fencer"
+end
