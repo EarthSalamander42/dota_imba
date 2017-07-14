ef494d8e9d4aa8c6c043904c5c8bbe260ca2152b
DESKTOP-SN8679M\jeizy <jeizyy30@hotmail.fr>
2017-07-14 14:10:46 +0200

    Fixed Top Scoreboard not displaying correclty for 4x3 and 16x10 ratio in 10v10 Talent Background now has opacity Added Imba XP Bar (Half-done, not working yet) Fixed double end screen showup

--- content/dota_addons/dota_imba/panorama/scripts/custom_game/multiteam_flyout_scoreboard_player.js
+++ content/dota_addons/dota_imba/panorama/scripts/custom_game/multiteam_flyout_scoreboard_player.js
@@ -5,115 +5,146 @@ var current_shown_tooltip_id = null;
 
 function HideToolTips()
 {
-    if( current_tooltip_timer )
-    {
-        $.CancelScheduled(current_tooltip_timer);
-        current_tooltip_timer = null;
-    }
-    current_shown_tooltip_id = null;
+	if( current_tooltip_timer )
+	{
+		$.CancelScheduled(current_tooltip_timer);
+		current_tooltip_timer = null;
+	}
+	current_shown_tooltip_id = null;
 
-    $.DispatchEvent("DOTAHideTextTooltip");
+	$.DispatchEvent("DOTAHideTextTooltip");
 }
 
 function ShowToolTip(button_id, text_normal, text_activated)
 {
-    var button = $.FindChildInContext(button_id)
-    if ( button )
-    {
-        current_tooltip_timer = null;
-        current_shown_tooltip_id = button_id;
+	var button = $.FindChildInContext(button_id)
+	if ( button )
+	{
+		current_tooltip_timer = null;
+		current_shown_tooltip_id = button_id;
 
-        var show_text = $.Localize( text_normal )
-        if ( button.BHasClass("Activated"))
-        {
-            show_text += " "+$.Localize( text_activated );
-        }
+		var show_text = $.Localize( text_normal )
+		if ( button.BHasClass("Activated"))
+		{
+			show_text += " "+$.Localize( text_activated );
+		}
 
-        $.DispatchEvent("DOTAShowTextTooltip", button, show_text);
-    }
+		$.DispatchEvent("DOTAShowTextTooltip", button, show_text);
+	}
 }
 
 function DelayedShowToolTipHelper(button_id, text_normal, text_activated)
 {
-    if( current_tooltip_timer )
-    {
-        $.CancelScheduled(current_tooltip_timer);
-    }
+	if( current_tooltip_timer )
+	{
+		$.CancelScheduled(current_tooltip_timer);
+	}
 
-    //Wait awhile before displaying tooltips
-    current_tooltip_timer = $.Schedule(0.5, function(){
-        ShowToolTip(button_id, text_normal, text_activated);
-    });
+	//Wait awhile before displaying tooltips
+	current_tooltip_timer = $.Schedule(0.5, function(){
+		ShowToolTip(button_id, text_normal, text_activated);
+	});
 }
 
 function ShowToolTipMute()
 {
-    DelayedShowToolTipHelper("#BtnMuteVoice", "#scoreboard_tool_tip_mute_player", "#scoreboard_tool_tip_active_mute_player");
+	DelayedShowToolTipHelper("#BtnMuteVoice", "#scoreboard_tool_tip_mute_player", "#scoreboard_tool_tip_active_mute_player");
 }
 
 function ShowToolTipShareUnit()
 {
-    DelayedShowToolTipHelper("#BtnShareUnit", "#scoreboard_tool_tip_share_unit", "#scoreboard_tool_tip_active_share_unit_hero");
+	DelayedShowToolTipHelper("#BtnShareUnit", "#scoreboard_tool_tip_share_unit", "#scoreboard_tool_tip_active_share_unit_hero");
 }
 
 function ShowToolTipShareHero()
 {
-    DelayedShowToolTipHelper("#BtnShareHero", "#scoreboard_tool_tip_share_hero", "#scoreboard_tool_tip_active_share_unit_hero");
+	DelayedShowToolTipHelper("#BtnShareHero", "#scoreboard_tool_tip_share_hero", "#scoreboard_tool_tip_active_share_unit_hero");
 }
 
 function ShowToolTipDisableHelp()
 {
-    DelayedShowToolTipHelper("#BtnDisableHelp", "#scoreboard_tool_tip_disable_help", "#scoreboard_tool_tip_active_disable_help");
+	DelayedShowToolTipHelper("#BtnDisableHelp", "#scoreboard_tool_tip_disable_help", "#scoreboard_tool_tip_active_disable_help");
 }
 
 function ToggleMute()
 {
-    var bol_reopen_tooltip = (current_shown_tooltip_id == "#BtnMuteVoice");
-    HideToolTips();
-    var playerId = $.GetContextPanel().GetAttributeInt( "player_id", -1 );
-    if ( Players.IsValidPlayerID(playerId) )
-    {
-        var muteStatus = !Game.IsPlayerMuted( playerId )
-        Game.SetPlayerMuted( playerId, muteStatus );
-    }
-    if(bol_reopen_tooltip){
-        ShowToolTipMute();
-    }
+	var bol_reopen_tooltip = (current_shown_tooltip_id == "#BtnMuteVoice");
+	HideToolTips();
+	var playerId = $.GetContextPanel().GetAttributeInt( "player_id", -1 );
+	if ( Players.IsValidPlayerID(playerId) )
+	{
+		var muteStatus = !Game.IsPlayerMuted( playerId )
+		Game.SetPlayerMuted( playerId, muteStatus );
+	}
+	if(bol_reopen_tooltip){
+		ShowToolTipMute();
+	}
 }
 
 function ToggleEvent(toggle_event_name)
 {
-    HideToolTips();
-    var playerId = $.GetContextPanel().GetAttributeInt( "player_id", -1 );
-    if ( Players.IsValidPlayerID(playerId) )
-    {
-        GameEvents.SendCustomGameEventToServer(toggle_event_name, { otherPlayerID : playerId })
-    }
+	HideToolTips();
+	var playerId = $.GetContextPanel().GetAttributeInt( "player_id", -1 );
+	if ( Players.IsValidPlayerID(playerId) )
+	{
+		GameEvents.SendCustomGameEventToServer(toggle_event_name, { otherPlayerID : playerId })
+	}
 }
 
 function ToggleShareUnit()
 {
-    var bol_reopen_tooltip = (current_shown_tooltip_id == "#BtnShareUnit");
-    ToggleEvent("toggle_share_unit")
-    if(bol_reopen_tooltip){
-        ShowToolTipShareUnit();
-    }
+	var bol_reopen_tooltip = (current_shown_tooltip_id == "#BtnShareUnit");
+	ToggleEvent("toggle_share_unit")
+	if(bol_reopen_tooltip){
+		ShowToolTipShareUnit();
+	}
 }
 
 function ToggleShareHero()
 {
-    var bol_reopen_tooltip = (current_shown_tooltip_id == "#BtnShareHero");
-    ToggleEvent("toggle_share_hero")
-    if(bol_reopen_tooltip){
-        ShowToolTipShareHero();
-    }
+	var bol_reopen_tooltip = (current_shown_tooltip_id == "#BtnShareHero");
+	ToggleEvent("toggle_share_hero")
+	if(bol_reopen_tooltip){
+		ShowToolTipShareHero();
+	}
 }
 
 function ToggleDisableHelp()
 {
-    var bol_reopen_tooltip = (current_shown_tooltip_id == "#BtnDisableHelp");
-    ToggleEvent("toggle_disable_player_help")
-    if(bol_reopen_tooltip){
-        ShowToolTipDisableHelp();
-    }
+	var bol_reopen_tooltip = (current_shown_tooltip_id == "#BtnDisableHelp");
+	ToggleEvent("toggle_disable_player_help")
+	if(bol_reopen_tooltip){
+		ShowToolTipDisableHelp();
+	}
 }
+
+/*
+function OnPlayerDataUpdated( table_name, key, data )
+{
+	UpdatePlayerImbaXP();
+}
+CustomNetTables.SubscribeNetTableListener( "player_table", OnPlayerDataUpdated )
+*/
+
+function UpdatePlayerImbaXP()
+{
+	$.Msg("Updating Player Data...")
+	var key = "ImbaXP"
+	var plyData = CustomNetTables.GetTableValue("player_table", key);
+
+	if ( plyData !== null )
+	{
+		if (plyData.value == "XP")
+		{
+			var nBossHP = plyData.number;
+			var bShowBossHP = plyData.number == 0 ? false : true;
+			$("#XPProgressBar").value = nBossHP / 100;
+//			$("#XPHP").visible = true;
+		}
+	}
+}
+
+(function()
+{
+    UpdatePlayerImbaXP();
+})();
