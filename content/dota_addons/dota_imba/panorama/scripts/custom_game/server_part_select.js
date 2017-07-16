b89f3be7f54c5b34e92692ed11e33e0ed936df50
MouJiaoZi <ww64646464@vip.qq.com>
2017-07-16 18:29:52 +0800

    particle select system part.3

--- content/dota_addons/dota_imba/panorama/scripts/custom_game/server_part_select.js
+++ content/dota_addons/dota_imba/panorama/scripts/custom_game/server_part_select.js
@@ -1,18 +1,55 @@
 
 
+
+function GetHUDRootUI_Server()
+{
+    var rootUI = $.GetContextPanel();
+    while(rootUI.id != "Hud" && rootUI.GetParent() != null){
+        rootUI = rootUI.GetParent();
+    }
+    return rootUI;
+}
+
+function GetUI_Server()
+{
+	return GetHUDRootUI_Server().FindChildTraverse("CustomUIRoot");
+}
+
+
+var PartSelectPage= null;
+
 function Server_Show_Select_Page()
 {
-	
-	$.Msg("!@#");
+	if(!PartSelectPage)
+	{
+		var parentPanel = GetUI_Server(); // the root panel of the current XML context
+		PartSelectPage = $.CreatePanel( "Panel", parentPanel, "ChildPanelID" );
+		PartSelectPage.BLoadLayout( "file://{resources}/layout/custom_game/server_select_page.xml", false, false );
+	}
+	else
+	{
+		if( PartSelectPage.invisible )
+		{
+			$.Msg("unhide ");
+			PartSelectPage.invisible=false;
+			PartSelectPage.SetHasClass("Hidden", false);
+		}
+		else
+		{
+			$.Msg("to hide");
+			PartSelectPage.invisible=true;
+			PartSelectPage.SetHasClass("Hidden", true);
+		}
+	}
 }
 
 
 function Server_Part_Select_Show_Tooltip()
 {
-    $.DispatchEvent("DOTAShowTitleTextTooltip",$("#select_button"), ":D", ":DDD");
+    $.DispatchEvent("DOTAShowTextTooltip",$("#select_button"), ":DDDD");
 }
 
 function Server_Part_Select_Hide_Tooltip()
 {
-    $.DispatchEvent( "DOTAHideTitleTextTooltip",$("#select_button") );
+    $.DispatchEvent( "DOTAHideTextTooltip",$("#select_button") );
 }
