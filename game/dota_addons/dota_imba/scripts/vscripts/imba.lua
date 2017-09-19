60b4b856cd8d5cc4e67305ef87955162d2929be2
DESKTOP-SN8679M\jeizy <jeizyy30@hotmail.fr>
2017-09-19 18:20:40 +0200

    * Diretide update
    Former-commit-id: f3da833dc6ca1ba2366cae9cc4820909f79c14ed

--- game/dota_addons/dota_imba/scripts/vscripts/imba.lua
+++ game/dota_addons/dota_imba/scripts/vscripts/imba.lua
@@ -1,6 +1,5 @@
 -- This is the primary barebones gamemode script and should be used to assist in initializing your game mode
 
-
 -- Set this to true if you want to see a complete debug output of all events/processes done by barebones
 -- You can also change the cvar 'barebones_spew' at any time to 1 or 0 for output/no output
 
