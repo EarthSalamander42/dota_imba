98468150e6714fd03a8b106de8c87347f6b286f1
IamInnocentX3 <dezumonodiron@Hotmail.com>
2017-10-07 10:54:18 +0800

    Kunkka fix
    Former-commit-id: 0149b16201d618033c829c996e10852fe8df1252

--- game/dota_addons/dota_imba/scripts/vscripts/hero/hero_kunkka.lua
+++ game/dota_addons/dota_imba/scripts/vscripts/hero/hero_kunkka.lua
@@ -650,8 +650,10 @@ function modifier_imba_kunkka_torrent_talent_thinker:OnCreated(keys)
         -- Ability properties
 		self.caster = self:GetCaster()
 		self.ability = self:GetAbility()
+		local min_interval = self.caster:FindTalentValue("special_bonus_imba_kunkka_4","min_interval") * 10
+		local max_interval = self.caster:FindTalentValue("special_bonus_imba_kunkka_4") * 10
 
-		self.tick = math.random() + math.random(0, self.caster:FindTalentValue("special_bonus_imba_kunkka_4"))
+		self.tick = (math.random() + math.random(min_interval, max_interval)) / 10
 		
 		-- Parameters
 		-- fuck you vectors
