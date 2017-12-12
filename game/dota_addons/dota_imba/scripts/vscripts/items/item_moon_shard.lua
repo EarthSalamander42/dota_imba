646abc8219bba517a091218afeb3bf9dc2a3adf8
MouJiaoZi <ww64646464@vip.qq.com>
2017-12-12 20:05:58 +0800

    fixed moon shard active sound emits globally
    Former-commit-id: 67ef74128d11e5c7587cdc8459fb609e2aa928ba

--- game/dota_addons/dota_imba/scripts/vscripts/items/item_moon_shard.lua
+++ game/dota_addons/dota_imba/scripts/vscripts/items/item_moon_shard.lua
@@ -65,7 +65,7 @@ end
 
 ]]
 
---[[ --
+--[[
 
 		Author: MouJiaoZi
 		Date: 2017.11.29
@@ -98,7 +98,7 @@ function item_imba_moon_shard:OnSpellStart()
 				target = nil
 			end
 		if target == caster then
-			EmitSoundOn("Item.MoonShard.Consume", caster)
+			EmitSoundOnClient("Item.MoonShard.Consume", caster:GetPlayerOwner())
 			local moon_buff = caster:FindModifierByName("modifier_item_imba_moon_shard_active")
 			if moon_buff then
 				moon_buff:SetStackCount(moon_buff:GetStackCount() + current_stacks)
@@ -108,8 +108,8 @@ function item_imba_moon_shard:OnSpellStart()
 			end
 			caster:RemoveItem(self)
 		else
-			EmitSoundOn("Item.MoonShard.Consume", caster)
-			EmitSoundOn("Item.MoonShard.Consume", target)
+			EmitSoundOnClient("Item.MoonShard.Consume", caster:GetPlayerOwner())
+			EmitSoundOnClient("Item.MoonShard.Consume", target:GetPlayerOwner())
 			local moon_buff = target:FindModifierByName("modifier_item_imba_moon_shard_active")
 			if moon_buff then
 				moon_buff:SetStackCount(moon_buff:GetStackCount() + 1)
