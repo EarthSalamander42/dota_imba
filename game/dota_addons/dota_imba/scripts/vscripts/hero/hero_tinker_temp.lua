8b3bc0c4ac3ebb4edf21979e586d77fe2f4617d6
MouJiaoZi <ww64646464@vip.qq.com>
2017-12-25 14:09:07 +0800

    tinker ulti
    Former-commit-id: 3bb1a5461ece53f4b8bac23e3581ed4d845f47e5

---
+++ game/dota_addons/dota_imba/scripts/vscripts/hero/hero_tinker_temp.lua
@@ -0,0 +1,68 @@
+tinker_rearm = tinker_rearm or class({})
+
+function tinker_rearm:IsHiddenWhenStolen() 		return false end
+function tinker_rearm:IsRefreshable() 			return true  end
+function tinker_rearm:IsStealable() 				return true  end
+function tinker_rearm:IsNetherWardStealable() 	return false end
+
+function tinker_rearm:OnAbilityPhaseStart()
+	EmitSoundOn("Hero_Tinker.RearmStart", self:GetCaster())
+	return true
+end
+
+function tinker_rearm:OnSpellStart()
+	if self:GetCaster():GetName() == "npc_dota_hero_tinker" then
+		local gesture = {ACT_DOTA_TINKER_REARM1,ACT_DOTA_TINKER_REARM2,ACT_DOTA_TINKER_REARM3}
+		self:GetCaster():StartGesture(gesture[math.min(self:GetLevel(),3)])
+	end
+	
+	EmitSoundOn("Hero_Tinker.Rearm", self:GetCaster())
+end
+
+function tinker_rearm:OnChannelFinish(bInter)
+	StopSoundOn("Hero_Tinker.Rearm", self:GetCaster())
+	local caster = self:GetCaster()
+	-- List of unrefreshable items
+	local forbidden_items = 
+							{
+							"item_imba_bloodstone",
+							"item_imba_arcane_boots",
+							"item_imba_mekansm",
+							"item_imba_mekansm_2",
+							"item_imba_guardian_greaves",
+							"item_imba_hand_of_midas",
+							"item_imba_white_queen_cape",
+							"item_black_king_bar",
+							"item_imba_refresher",
+							"item_imba_necronomicon",
+							"item_imba_necronomicon_2",
+							"item_imba_necronomicon_3",
+							"item_imba_necronomicon_4",
+							"item_imba_necronomicon_5",
+							"item_imba_skadi",
+							"item_imba_sphere"
+							}
+	if not bInter then
+		for i = 0, 23 do
+			local ability = caster:GetAbilityByIndex(i)
+			if ability and ability.IsRefreshable and ability:IsRefreshable() then
+				ability:EndCooldown()
+			end
+		end
+		for i = 0, 5 do
+			local item = caster:GetItemInSlot(i)
+			local canRefresh = true
+			if item then
+				for _, unre in pairs(forbidden_items) do
+					if item:GetAbilityName() == unre then
+						canRefresh = false
+						break
+					end
+				end
+				if canRefresh then
+					item:EndCooldown()
+				end
+			end
+		end
+	end
+end
