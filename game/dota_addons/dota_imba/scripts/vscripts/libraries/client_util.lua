8eddd298b4dc2d9f88e6334a6566b8b6e93b16bf
MouJiaoZi <ww64646464@vip.qq.com>
2017-12-30 13:25:36 +0800

    try to improve talent system
    Former-commit-id: cffdb596ec4a99eb82890bb79337452920129acc

--- game/dota_addons/dota_imba/scripts/vscripts/libraries/client_util.lua
+++ game/dota_addons/dota_imba/scripts/vscripts/libraries/client_util.lua
@@ -71,8 +71,11 @@ end
 function CreateEmptyTalents(hero)
 	for i=1,8 do
 		LinkLuaModifier("modifier_special_bonus_imba_"..hero.."_"..i, "hero/hero_"..hero, LUA_MODIFIER_MOTION_NONE)  
-		local class = "modifier_special_bonus_imba_"..hero.."_".. i.." = class({IsHidden = function(self) return true end, RemoveOnDeath = function(self) return false end})"    
+		local class = "modifier_special_bonus_imba_"..hero.."_"..i.." = class({IsHidden = function(self) return false end, RemoveOnDeath = function(self) return false end, AllowIllusionDuplicate = function(self) return true end, GetTexture = function(self) return 'naga_siren_mirror_image' end})"  
 		load(class)()
+
+		local class2 = "special_bonus_imba_"..hero.."_"..i.." = class({GetIntrinsicModifierName = function(self) return 'modifier_special_bonus_imba_"..hero.."_"..i.."' end})"
+		load (class2)()
 	end
 end
 
