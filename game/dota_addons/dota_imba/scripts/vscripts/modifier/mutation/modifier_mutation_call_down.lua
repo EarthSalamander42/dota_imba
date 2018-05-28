130f69918ac78aa25a94f1a7c6825800d8e6f94b
MouJiaoZi <ww64646464@vip.qq.com>
2018-05-28 16:55:50 +0800

    call down 2

--- game/dota_addons/dota_imba/scripts/vscripts/modifier/mutation/modifier_mutation_call_down.lua
+++ game/dota_addons/dota_imba/scripts/vscripts/modifier/mutation/modifier_mutation_call_down.lua
@@ -23,22 +23,71 @@ thinker_call_down_2 = class({})
 
 function thinker_call_down:OnCreated(keys)
 	if not IsServer() then return end
+	self.radius = keys.radius or 400
 	self.damage_1 = math.max(50, 50 * (GetGameTime() / 60)) -- first explosion damage
 	self.slow_1 = 30 -- first explosion slow down
 	self.slow_duration = 3.0 -- slow down duration
 end
 
 function thinker_call_down:OnDestroy()
-
+	if not IsServer() then return end
+	local units = FindUnitsInRadius(1,
+									self.GetParent():GetAbsOrigin(),
+									nil,
+									self.radius,
+									DOTA_UNIT_TARGET_TEAM_BOTH,
+									DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_BASIC,
+									DOTA_UNIT_TARGET_FLAG_NONE,
+									FIND_ANY_ORDER,
+									false)
+	for _,unit in pairs(units) do
+		local damageTable = {
+							victim = unit,
+							attacker = self:GetParent(),
+							damage = self.damage_1,
+							damage_type = DAMAGE_TYPE_MAGICAL,
+							damage_flags = DOTA_DAMAGE_FLAG_NONE, --Optional.
+							ability = nil, --Optional.
+							}
+		ApplyDamage(damageTable)
+		unit:AddNewModifier(self.GetParent(), nil, "modifier_call_down_slow", {duration = self.slow_duration, slow = self.slow_1})
+	end
+end
 
 function thinker_call_down_2:OnCreated(keys)
 	if not IsServer() then return end
 	----------
 	-- Create Particle
 	----------
+	self.radius = keys.radius or 400
 	self.damage_2 = math.max(75, 75 * (GetGameTime() / 60))	-- second explosion damage
 	self.slow_2 = 70 -- second explosion slow down
 	self.slow_duration = 3.0 -- slow down duration
+end
+
+function thinker_call_down_2:OnDestroy()
+	if not IsServer() then return end
+	local units = FindUnitsInRadius(1,
+									self.GetParent():GetAbsOrigin(),
+									nil,
+									self.radius,
+									DOTA_UNIT_TARGET_TEAM_BOTH,
+									DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_BASIC,
+									DOTA_UNIT_TARGET_FLAG_NONE,
+									FIND_ANY_ORDER,
+									false)
+	for _,unit in pairs(units) do
+		local damageTable = {
+							victim = unit,
+							attacker = self:GetParent(),
+							damage = self.damage_2,
+							damage_type = DAMAGE_TYPE_MAGICAL,
+							damage_flags = DOTA_DAMAGE_FLAG_NONE, --Optional.
+							ability = nil, --Optional.
+							}
+		ApplyDamage(damageTable)
+		unit:AddNewModifier(self.GetParent(), nil, "modifier_call_down_slow", {duration = self.slow_duration, slow = self.slow_2})
+	end
 end
 
 modifier_call_down_slow = class({})
