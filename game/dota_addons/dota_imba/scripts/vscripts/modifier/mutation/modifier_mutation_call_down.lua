1c370b05c4fdb3587310df78156c5f236d94a5d1
MouJiaoZi <ww64646464@vip.qq.com>
2018-06-15 23:14:41 +0800

    mutation

--- game/dota_addons/dota_imba/scripts/vscripts/modifier/mutation/modifier_mutation_call_down.lua
+++ game/dota_addons/dota_imba/scripts/vscripts/modifier/mutation/modifier_mutation_call_down.lua
@@ -5,17 +5,22 @@ function modifier_mutation_call_down:IsHidden() return true end
 
 function modifier_mutation_call_down:OnCreated()
 	if not IsServer() then return end
-	self.interval = 2.0	-- This is Call Down's interval to FIRST explosion
-	self.radius = 400 -- Radius
-	self:StartIntervalThink(1.0)
+	self.interval = 2.0	-- This is Call Down's interval to FIRST explosion DO NOT CHANGE THIS
+	self.radius = 600 -- Radius
+	self:StartIntervalThink(0.1)
+	self.parent = self:GetParent()
 end
 
 function modifier_mutation_call_down:OnIntervalThink()
 	local x = math.random(-8000, 8000)
 	local y = math.random(-8000, 8000)
 	local pos = GetGroundPosition(Vector(x, y, 0), nil)
-	CreateModifierThinker(self, nil, "thinker_call_down", {duration = self.interval, raidus = self.radius}, pos, -1, false)
-	CreateModifierThinker(self, nil, "thinker_call_down_2", {duration = self.interval + 2.0, raidus = self.radius}, pos, -1, false)
+	CreateModifierThinker(self, nil, "thinker_call_down", {duration = self.interval, raidus = self.radius}, pos, 0, false)
+	local thinker = CreateModifierThinker(self, nil, "thinker_call_down_2", {duration = self.interval + 2.0, raidus = self.radius}, pos, 0, false)
+	local particle = ParticleManager:CreateParticle("particles/units/heroes/hero_gyrocopter/gyro_calldown_marker.vpcf", PATTACH_WORLDORIGIN, self.parent)
+	ParticleManager:SetParticleControl(particle, 0, pos)
+	ParticleManager:SetParticleControl(particle, 1, Vector(self.radius, 0, 0-self.radius))
+	self:AddParticle(particle, false, false, -1, false, false)
 end
 
 thinker_call_down = class({})
@@ -24,15 +29,19 @@ thinker_call_down_2 = class({})
 function thinker_call_down:OnCreated(keys)
 	if not IsServer() then return end
 	self.radius = keys.radius or 400
-	self.damage_1 = math.max(50, 50 * (GetGameTime() / 60)) -- first explosion damage
+	self.damage_1 = math.max(50, 50 * (GameRules:GetGameTime() / 60)) -- first explosion damage
 	self.slow_1 = 30 -- first explosion slow down
 	self.slow_duration = 3.0 -- slow down duration
 end
 
 function thinker_call_down:OnDestroy()
 	if not IsServer() then return end
+	local particle = ParticleManager:CreateParticle("particles/units/heroes/hero_gyrocopter/gyro_calldown_explosion.vpcf", PATTACH_WORLDORIGIN, self.parent)
+	ParticleManager:SetParticleControl(particle, 3, self:GetParent():GetAbsOrigin())
+	ParticleManager:SetParticleControl(particle, 5, Vector(self.radius, 0, 0))
+	self:AddParticle(particle, false, false, -1, false, false)
 	local units = FindUnitsInRadius(1,
-									self.GetParent():GetAbsOrigin(),
+									self:GetParent():GetAbsOrigin(),
 									nil,
 									self.radius,
 									DOTA_UNIT_TARGET_TEAM_BOTH,
@@ -50,7 +59,7 @@ function thinker_call_down:OnDestroy()
 							ability = nil, --Optional.
 							}
 		ApplyDamage(damageTable)
-		unit:AddNewModifier(self.GetParent(), nil, "modifier_call_down_slow", {duration = self.slow_duration, slow = self.slow_1})
+		unit:AddNewModifier(self:GetParent(), nil, "modifier_call_down_slow", {duration = self.slow_duration, slow = self.slow_1})
 	end
 end
 
@@ -60,15 +69,19 @@ function thinker_call_down_2:OnCreated(keys)
 	-- Create Particle
 	----------
 	self.radius = keys.radius or 400
-	self.damage_2 = math.max(75, 75 * (GetGameTime() / 60))	-- second explosion damage
+	self.damage_2 = math.max(75, 75 * (GameRules:GetGameTime() / 60))	-- second explosion damage
 	self.slow_2 = 70 -- second explosion slow down
 	self.slow_duration = 3.0 -- slow down duration
 end
 
 function thinker_call_down_2:OnDestroy()
 	if not IsServer() then return end
+	local particle = ParticleManager:CreateParticle("particles/units/heroes/hero_gyrocopter/gyro_calldown_explosion.vpcf", PATTACH_WORLDORIGIN, self.parent)
+	ParticleManager:SetParticleControl(particle, 3, self:GetParent():GetAbsOrigin())
+	ParticleManager:SetParticleControl(particle, 5, Vector(self.radius, 0, 0))
+	self:AddParticle(particle, false, false, -1, false, false)
 	local units = FindUnitsInRadius(1,
-									self.GetParent():GetAbsOrigin(),
+									self:GetParent():GetAbsOrigin(),
 									nil,
 									self.radius,
 									DOTA_UNIT_TARGET_TEAM_BOTH,
@@ -86,7 +99,7 @@ function thinker_call_down_2:OnDestroy()
 							ability = nil, --Optional.
 							}
 		ApplyDamage(damageTable)
-		unit:AddNewModifier(self.GetParent(), nil, "modifier_call_down_slow", {duration = self.slow_duration, slow = self.slow_2})
+		unit:AddNewModifier(self:GetParent(), nil, "modifier_call_down_slow", {duration = self.slow_duration, slow = self.slow_2})
 	end
 end
 
--- game/dota_addons/dota_imba/scripts/vscripts/modifier/mutation/modifier_mutation_void_path.lua
+++ game/dota_addons/dota_imba/scripts/vscripts/modifier/mutation/modifier_mutation_void_path.lua
@@ -1,4 +1,4 @@
-
+LinkLuaModifier("modifier_imba_faceless_void_chronosphere_handler", "hero/hero_faceless_void.lua", LUA_MODIFIER_MOTION_NONE)
 ---------------------------------
 -----	Chronosphere Aura	-----
 ---------------------------------
@@ -19,7 +19,7 @@ function modifier_chronosphere_aura:GetAuraSearchFlags()
 	return DOTA_UNIT_TARGET_FLAG_MAGIC_IMMUNE_ENEMIES + DOTA_UNIT_TARGET_FLAG_INVULNERABLE end
 
 function modifier_chronosphere_aura:GetAuraSearchType()
-	return DOTA_UNIT_TARGET_CREEP + DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_BUILDING + DOTA_UNIT_TARGET_OTHER end
+	return DOTA_UNIT_TARGET_ALL end
 
 function modifier_chronosphere_aura:GetModifierAura()
 	return "modifier_imba_faceless_void_chronosphere_handler" end
@@ -31,8 +31,8 @@ end
 function modifier_chronosphere_aura:OnCreated()
 	if IsServer() then
 		self.parent = self:GetParent()
-		self.radius = 600	-- This is Radius
-		local particle = ParticleManager:CreateParticle("particles/units/heroes/hero_faceless_void/faceless_void_chronosphere.vpcf", PATTACH_WORLDORIGIN, self.parent)
+		self.radius = math.random(200,400)	-- This is Radius
+		local particle = ParticleManager:CreateParticle("particles/econ/items/faceless_void/faceless_void_mace_of_aeons/fv_chronosphere_aeons.vpcf", PATTACH_WORLDORIGIN, self.parent)
 		ParticleManager:SetParticleControl(particle, 0, self.parent:GetAbsOrigin())
 		ParticleManager:SetParticleControl(particle, 1, Vector(self.radius, self.radius, self.radius))
 		self:AddParticle(particle, false, false, -1, false, false)
@@ -44,12 +44,14 @@ modifier_mutation_void_path = class({})
 function modifier_mutation_void_path:IsHidden() return true end
 
 function modifier_mutation_void_path:OnCreated()
-	self:StartIntervalThink(1.0)
+	self:StartIntervalThink(0.1)
 end
 
 function modifier_mutation_void_path:OnIntervalThink()
 	local x = math.random(-8000, 8000)
 	local y = math.random(-8000, 8000)
 	local pos = GetGroundPosition(Vector(x, y, 0), nil)
-	CreateModifierThinker(self, nil, "modifier_chronosphere_aura", {duration = 5.0}, pos, -1, false)	-- This is Duration
+	local sound_cast = "Hero_FacelessVoid.Chronosphere"
+	self:GetParent():EmitSound(sound_cast)
+	CreateModifierThinker(self:GetParent(), nil, "modifier_chronosphere_aura", {duration = 3.0}, pos, 0, false)	-- This is Duration
 end
