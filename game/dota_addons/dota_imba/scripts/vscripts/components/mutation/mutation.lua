416a95ec504235f397cf966e18a984342291cb14
AV <altivu@gmail.com>
2018-07-05 16:24:37 +0200

    Re-enabled track as per suthern's request

--- game/dota_addons/dota_imba/scripts/vscripts/components/mutation/mutation.lua
+++ game/dota_addons/dota_imba/scripts/vscripts/components/mutation/mutation.lua
@@ -54,13 +54,13 @@ function Mutation:Init()
 	IMBA_MUTATION_PERIODIC_SPELLS = {}
 	IMBA_MUTATION_PERIODIC_SPELLS[1] = {"sun_strike", "Sunstrike", "Red", -1}
 	IMBA_MUTATION_PERIODIC_SPELLS[2] = {"thundergods_wrath", "Thundergod's Wrath", "Red", -1}
-	--IMBA_MUTATION_PERIODIC_SPELLS[3] = {"track", "Track", "Red", 20.0}
+    IMBA_MUTATION_PERIODIC_SPELLS[3] = {"track", "Track", "Red", 20.0}
 	IMBA_MUTATION_PERIODIC_SPELLS[4] = {"rupture", "Rupture", "Red", 10.0}
 	IMBA_MUTATION_PERIODIC_SPELLS[5] = {"torrent", "Torrent", "Red", -1}
 	IMBA_MUTATION_PERIODIC_SPELLS[6] = {"cold_feet", "Cold Feet", "Red", 4.0}
 	IMBA_MUTATION_PERIODIC_SPELLS[7] = {"stampede", "Stampede", "Green", 5.0}
 	IMBA_MUTATION_PERIODIC_SPELLS[8] = {"bloodlust", "Bloodlust", "Green", 30.0}
-	IMBA_MUTATION_PERIODIC_SPELLS[3] = {"aphotic_shield", "Aphotic Shield", "Green", 15.0}
+	IMBA_MUTATION_PERIODIC_SPELLS[9] = {"aphotic_shield", "Aphotic Shield", "Green", 15.0}
     
 --[     TO DO
 --	"telekinesis",
