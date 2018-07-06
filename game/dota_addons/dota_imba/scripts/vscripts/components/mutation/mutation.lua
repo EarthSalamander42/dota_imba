c3b2f0fc1e928f0fa8b034a6783c266bb2912416
AV <altivu@gmail.com>
2018-07-06 04:16:29 +0200

    Added River Flows to rotation

--- game/dota_addons/dota_imba/scripts/vscripts/components/mutation/mutation.lua
+++ game/dota_addons/dota_imba/scripts/vscripts/components/mutation/mutation.lua
@@ -46,8 +46,8 @@ function Mutation:Init()
 	LinkLuaModifier("modifier_mutation_bloodlust", "components/mutation/modifiers/periodic_spellcast/modifier_mutation_bloodlust.lua", LUA_MODIFIER_MOTION_NONE )
 	LinkLuaModifier("modifier_mutation_aphotic_shield", "components/mutation/modifiers/periodic_spellcast/modifier_mutation_aphotic_shield.lua", LUA_MODIFIER_MOTION_NONE )
 
-	LinkLuaModifier("modifier_river_flows", "modifier/mutation/modifier_river_flows.lua", LUA_MODIFIER_MOTION_NONE )
-	LinkLuaModifier("modifier_sticky_river", "modifier/mutation/modifier_sticky_river.lua", LUA_MODIFIER_MOTION_NONE )
+	LinkLuaModifier("modifier_mutation_river_flows", "components/mutation/modifiers/modifier_mutation_river_flows.lua", LUA_MODIFIER_MOTION_NONE )
+	LinkLuaModifier("modifier_mutation_sticky_river", "components/mutation/modifiers/modifier_mutation_sticky_river.lua", LUA_MODIFIER_MOTION_NONE )
 
 	Mutation:ChooseMutation("positive", POSITIVE_MUTATION_LIST, 6 - 1) -- -1 because index is 0
 	Mutation:ChooseMutation("negative", NEGATIVE_MUTATION_LIST, 3 - 1)
@@ -144,7 +144,7 @@ function Mutation:ChooseMutation(type, table, count)
 			if IsInToolsMode() then
 				IMBA_MUTATION["positive"] = "super_fervor"
 				IMBA_MUTATION["negative"] = "periodic_spellcast"
-				IMBA_MUTATION["terrain"] = "speed_freaks"
+				IMBA_MUTATION["terrain"] = "river_flows"
 			end
 
 			return
@@ -409,7 +409,7 @@ function Mutation:OnHeroSpawn(hero)
 	end
 
 	if IMBA_MUTATION["terrain"] == "river_flows" then
-		hero:AddNewModifier(hero, nil, "modifier_river_flows", {})
+		hero:AddNewModifier(hero, nil, "modifier_mutation_river_flows", {})
     end
 
 	if IMBA_MUTATION["terrain"] == "sticky_river" then
