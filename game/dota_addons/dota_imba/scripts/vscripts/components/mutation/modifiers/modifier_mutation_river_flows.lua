3512ae367b4b12e3603b805950e936b8872297cb
AV <altivu@gmail.com>
2018-07-06 04:15:47 +0200

    Uploaded River Flows lua file (MS at 1,000 for now)

---
+++ game/dota_addons/dota_imba/scripts/vscripts/components/mutation/modifiers/modifier_mutation_river_flows.lua
@@ -0,0 +1,62 @@
+LinkLuaModifier("modifier_mutation_river_flows_boost", "components/mutation/modifiers/modifier_mutation_river_flows.lua", LUA_MODIFIER_MOTION_NONE )
+
+modifier_mutation_river_flows = modifier_mutation_river_flows or class({})
+function modifier_mutation_river_flows:IsHidden() return true end
+function modifier_mutation_river_flows:IsDebuff() return false end
+function modifier_mutation_river_flows:IsPurgable() return false end
+
+function modifier_mutation_river_flows:OnCreated()
+	if IsServer() then
+		self:StartIntervalThink(0.1)
+	end
+end
+
+function modifier_mutation_river_flows:OnIntervalThink()
+	if IsServer() then
+		local parent = self:GetParent()
+
+		if self:IsInRiver() then
+			if not parent:HasModifier("modifier_mutation_river_flows_boost") then
+				parent:AddNewModifier(parent, nil, "modifier_mutation_river_flows_boost", {})
+			end
+		else
+			parent:RemoveModifierByName("modifier_mutation_river_flows_boost")
+		end
+	end
+end
+
+function modifier_mutation_river_flows:IsInRiver()
+	local parent = self:GetParent()
+	local origin = parent:GetAbsOrigin()
+	if parent:GetUnitName() == "npc_dota_elder_titan_ancestral_spirit" or parent:GetUnitName() == "npc_dota_gyrocopter_homing_missile" then return end
+
+	if origin.z < 160 and parent:HasGroundMovementCapability() then
+		return true
+	else
+		return false
+	end
+end
+
+modifier_mutation_river_flows_boost = class({})
+function modifier_mutation_river_flows_boost:IsHidden() return false end
+function modifier_mutation_river_flows_boost:IsDebuff() return false end
+function modifier_mutation_river_flows_boost:IsPurgable() return false end
+
+function modifier_mutation_river_flows_boost:GetTexture()
+	return "morphling_waveform"
+end
+
+function modifier_mutation_river_flows_boost:GetEffectName()
+	return "particles/item/boots/haste_boots_speed_boost.vpcf"
+end
+
+function modifier_mutation_river_flows_boost:DeclareFunctions()
+	local funcs = {
+		MODIFIER_PROPERTY_MOVESPEED_ABSOLUTE_MIN
+	}
+	return funcs
+end
+
+function modifier_mutation_river_flows_boost:GetModifierMoveSpeed_AbsoluteMin()
+	return 1000 -- OSFrog
+end
