02cde19d20316659a6859dd2dc658aa5e8d69689
AV <altivu@gmail.com>
2018-07-22 04:11:00 +0200

    Added some exception logic to account for some outliers:
    - RemoveOnDeath() to return false so Rubick doesn't lose modifier
    - Shadow Blade / Silver Edge modifier checks for InRiver so they can still get modifier while invis

--- game/dota_addons/dota_imba/scripts/vscripts/components/mutation/modifiers/modifier_mutation_river_flows.lua
+++ game/dota_addons/dota_imba/scripts/vscripts/components/mutation/modifiers/modifier_mutation_river_flows.lua
@@ -4,6 +4,7 @@ modifier_mutation_river_flows = modifier_mutation_river_flows or class({})
 function modifier_mutation_river_flows:IsHidden() return true end
 function modifier_mutation_river_flows:IsDebuff() return false end
 function modifier_mutation_river_flows:IsPurgable() return false end
+function modifier_mutation_river_flows:RemoveOnDeath() return false end
 
 function modifier_mutation_river_flows:OnCreated()
 	if IsServer() then
@@ -30,7 +31,8 @@ function modifier_mutation_river_flows:IsInRiver()
 	local origin = parent:GetAbsOrigin()
 	if parent:GetUnitName() == "npc_dota_elder_titan_ancestral_spirit" or parent:GetUnitName() == "npc_dota_gyrocopter_homing_missile" then return end
 
-	if origin.z < 160 and parent:HasGroundMovementCapability() then
+	-- Add additional logic to allow shadow blade / silver edge usage to also be in check for river
+	if origin.z < 160 and (parent:HasGroundMovementCapability() or parent:HasModifier("modifier_item_imba_shadow_blade_invis") or parent:HasModifier("modifier_item_imba_silver_edge_invis")) then
 		return true
 	else
 		return false
