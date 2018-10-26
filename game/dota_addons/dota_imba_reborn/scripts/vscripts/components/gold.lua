2036396e64245ad2b7adc8d986b86bd86b7c1998
DESKTOP-HFL3SLE\jeizy <jeizyy30@hotmail.fr>
2018-10-26 15:50:21 +0200

    * Fixed gold system script outdated
    Former-commit-id: 58ec355dbe9c6d06176d84623764c81566783314

--- game/dota_addons/dota_imba_reborn/scripts/vscripts/components/gold.lua
+++ game/dota_addons/dota_imba_reborn/scripts/vscripts/components/gold.lua
@@ -5,15 +5,13 @@ end
 function GoldSystem:OnHeroDeath(killer, victim)
 	local custom_gold_bonus = tonumber(CustomNetTables:GetTableValue("game_options", "bounty_multiplier")["1"])
 	local base_gold_bounty = 110 * (custom_gold_bonus / 100)
-	local level_difference = killer:GetLevel() - victim:GetLevel()
+	local level_difference = victim:GetLevel() - killer:GetLevel()
+	print(level_difference)
 	local level_bonus = 100 * math.max(level_difference, 0)
-	if killer:GetLevel() >= victim:GetLevel() then level_bonus = 0 end
 	if not victim.killstreak then victim.killstreak = 0 end
 	local kill_streak_with_limit = math.min(victim.killstreak, 15)
 	local streak_bonus = 100 * math.sqrt(kill_streak_with_limit) * kill_streak_with_limit
 	local kill_gold = math.floor(base_gold_bounty + level_bonus + streak_bonus)
-
-	print(base_gold_bounty, streak_value, streak_diff, kill_gold)
 
 	if not killer:IsRealHero() then killer = nil end
 
@@ -27,9 +25,6 @@ function GoldSystem:OnHeroDeath(killer, victim)
 			kill_gold = kill_gold + 150
 		end
 
-		SendOverheadEventMessage(killer:GetPlayerOwner(), OVERHEAD_ALERT_GOLD, victim, kill_gold, nil)
-		killer:ModifyGold(kill_gold, true, DOTA_ModifyGold_HeroKill)
-
 		local victim_team_networth = 0
 
 		for _, hero in pairs(HeroList:GetAllHeroes()) do
@@ -41,24 +36,41 @@ function GoldSystem:OnHeroDeath(killer, victim)
 		local average_victim_team_networth = victim_team_networth / PlayerResource:GetPlayerCountForTeam(victim:GetTeamNumber())
 		local assisters = FindUnitsInRadius(killer:GetTeamNumber(), killer:GetAbsOrigin(), nil, 1300, DOTA_UNIT_TARGET_TEAM_FRIENDLY, DOTA_UNIT_TARGET_HERO, DOTA_UNIT_TARGET_FLAG_NOT_ILLUSIONS, FIND_ANY_ORDER, false)
 
+		local aoe_gold_for_player
 		for _, assister in pairs(assisters) do
 			local base_aoe_gold = kill_gold / #assisters
 			local networth_bonus = math.max(average_victim_team_networth - assister:GetNetWorth(), 0) * 0.05
-			local aoe_gold_for_player = math.floor(base_aoe_gold + networth_bonus)
+			aoe_gold_for_player = math.floor(base_aoe_gold + networth_bonus)
+
+			if assister == killer then
+				SendOverheadEventMessage(killer:GetPlayerOwner(), OVERHEAD_ALERT_GOLD, victim, kill_gold + aoe_gold_for_player, nil)
+				killer:ModifyGold(kill_gold + aoe_gold_for_player, true, DOTA_ModifyGold_HeroKill)
+			else
+				SendOverheadEventMessage(assister:GetPlayerOwner(), OVERHEAD_ALERT_GOLD, victim, aoe_gold_for_player, nil)
+				assister:ModifyGold(aoe_gold_for_player, true, DOTA_ModifyGold_HeroKill)
+			end
 		end
 
-		CombatEvents("kill", "hero_kill", victim, killer, kill_gold)
+--		print(base_gold_bounty)
+--		print(level_bonus)
+--		print(streak_bonus)
+--		print(kill_gold)
+--		print(aoe_gold_for_player)
+
+		CombatEvents("kill", "hero_kill", victim, killer, kill_gold + aoe_gold_for_player)
 	else
 		local victim_attacker_count = victim:GetNumAttackers()
 
 		print("Attackers: "..victim_attacker_count)
 		if victim_attacker_count == 0 then
+			-- If there's no attackers and the hero didn't suicided or denied himself, grant gold to the enemy team
 			for _, hero in pairs(HeroList:GetAllHeroes()) do
 				print(kill_gold / PlayerResource:GetPlayerCountForTeam(hero:GetTeamNumber()), kill_gold)
 				SendOverheadEventMessage(hero:GetPlayerOwner(), OVERHEAD_ALERT_GOLD, victim, kill_gold / PlayerResource:GetPlayerCountForTeam(hero:GetTeamNumber()), nil)
 				hero:ModifyGold(kill_gold / PlayerResource:GetPlayerCountForTeam(hero:GetTeamNumber()), true, DOTA_ModifyGold_HeroKill)
 			end
 		else
+			-- if there are assisters but no killer (e.g: dead by tower) then grant gold to assisters
 			for _, attacker in pairs(HeroList:GetAllHeroes()) do
 				for i = 0, victim_attacker_count -1 do
 					if attacker:GetPlayerID() == victim:GetAttacker(i) then
