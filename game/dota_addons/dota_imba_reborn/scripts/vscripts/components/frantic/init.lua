49ae0205e7d8b6deed4babe8332d55b002b0fe2e
EarthSalamander #42 <jeizyy30@hotmail.fr>
2019-01-19 06:09:35 +0100

    Fixed infinite gold bug again + frantic not setup on first spawn

--- game/dota_addons/dota_imba_reborn/scripts/vscripts/components/frantic/init.lua
+++ game/dota_addons/dota_imba_reborn/scripts/vscripts/components/frantic/init.lua
@@ -1,13 +1,13 @@
 LinkLuaModifier("modifier_frantic", "components/modifiers/mutation/modifier_frantic.lua", LUA_MODIFIER_MOTION_NONE )
 
 ListenToGameEvent('game_rules_state_change', function(keys)
+--	print("Gamemode:", GameRules:GetCustomGameDifficulty())
+	if GameRules:GetCustomGameDifficulty() ~= 3 then
+		return
+	end
+
 	if GameRules:State_Get() >= DOTA_GAMERULES_STATE_HERO_SELECTION then
---		print("Gamemode:", GameRules:GetCustomGameDifficulty())
-		if GameRules:GetCustomGameDifficulty() ~= 3 then
-			return
-		else
-			require("components/frantic/settings")
-			require('components/frantic/events')
-		end		
+		require("components/frantic/settings")
+		require('components/frantic/events')
 	end
 end, nil)
