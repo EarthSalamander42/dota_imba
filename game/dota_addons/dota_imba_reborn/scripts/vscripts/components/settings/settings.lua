0b7b4d15c46d739a25e99fb78bcf0cf19fc3797b
AltiV <altivu@gmail.com>
2019-06-18 17:32:31 -0400

    Settings

--- game/dota_addons/dota_imba_reborn/scripts/vscripts/components/settings/settings.lua
+++ game/dota_addons/dota_imba_reborn/scripts/vscripts/components/settings/settings.lua
@@ -14,7 +14,7 @@
 
 CUSTOM_GAME_TYPE = "IMBA"
 
-GAME_VERSION = "7.14d"
+GAME_VERSION = "7.14e"
 CustomNetTables:SetTableValue("game_options", "game_version", {value = GAME_VERSION})
 CustomNetTables:SetTableValue("game_options", "gamemode", {1})
 
