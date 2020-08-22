e009c2c20fb4bbc9a77a20b8d3587179f142c887
EarthSalamander <jeizyy30@hotmail.fr>
2020-08-23 01:38:14 +0200

    more custom tooltips tests

--- content/dota_addons/dota_imba_reborn/panorama/scripts/custom_game/vanilla_link_tooltips.js
+++ content/dota_addons/dota_imba_reborn/panorama/scripts/custom_game/vanilla_link_tooltips.js
@@ -16,84 +16,131 @@ function GetDotaHud() {
 	} catch (e) {}
 }
 
-function PlainText(text) 
-{
-	var ability_splits = text.split(": ")
-	if(ability_splits != null && ability_splits.length == 2) 
-	{
-		return text.replace(ability_splits[1], "<font color='#FFFFFF'>" + ability_splits[1] + "</font>\n<br></br>")
-	}
-	else 
-	{
-		print("Failed to convert tooltip text: " + text)
-	}
-}
-
 var DotaHud = GetDotaHud();
 
-function init_invoker_tooltips() {
-	// Handle Invoked spell in slot 4
-	var ability3 = DotaHud.FindChildTraverse("Ability3");
-	ability3.SetPanelEvent(
-		"onmouseover", 
-		function(){
-			ColorizeInvokerTooltip(3)
-		}
-	)
+function AddHeroTooltips(slot_number) {
+	if (DotaHud.FindChildTraverse("AbilitiesAndStatBranch").BHasClass("npc_dota_hero_abaddon")) {
+		var hero = Entities.GetAllEntitiesByName("npc_dota_hero_abaddon")[0] || null;
+		var ability_name = null;
 
-	ability3.SetPanelEvent(
-		"onmouseout", 
-		function(){
-//			ToggleInvokerTooltip()
-		}
-	)
-	
-	// Handle Invoked spell in slot 5
-	var ability4 = DotaHud.FindChildTraverse("Ability4");
-	if (ability4) {
+		if (hero != null) {
+			var ability_level = 0;
 
-		ability4.SetPanelEvent(
-			"onmouseover", 
-			function(){
-				ColorizeInvokerTooltip(4)
+			if (slot_number <= 6) {
+				var ability_slot = Entities.GetAbility(hero, slot_number);
+				ability_level = Abilities.GetLevel(ability_slot);
+
+				if (ability_slot) {
+					ability_name = Abilities.GetAbilityName(ability_slot).substring(5);
+/*
+					if (ability_name && ability_name.indexOf('invoker_empty1') > -1) {
+						return
+					}
+*/
+				}					
 			}
-		)
 
-		ability4.SetPanelEvent(
-			"onmouseout", 
-			function(){
-				ToggleInvokerTooltip()
+//			print(ability_name)
+
+			var AbilityTooltip = DotaHud.FindChildTraverse("DOTAAbilityTooltip");
+
+			if (AbilityTooltip != null) {
+				var AbilityText = $.Localize("DOTA_Tooltip_ability_" + ability_name + "_Description");
+				var AbilityExtraAttributes = AbilityTooltip.FindChildTraverse('AbilityExtraAttributes');
+				var ability_text_rows =  AbilityText.split("\n");
+
+				var AbilityCooldown = AbilityTooltip.FindChildTraverse('AbilityCooldown');
+				AbilityCooldown.text = SetKeyValueLevel(ability_level, AbilityCooldown.text);
+
+				var AbilityManaCost = AbilityTooltip.FindChildTraverse('AbilityManaCost');
+
+				print(AbilityText)
+
+				var new_ability_text = "";
+
+				if (ability_text_rows != null && ability_text_rows.length > 0) {
+//					new_ability_text = new_ability_text + AbilityText;
+
+					var container = DotaHud.FindChildTraverse("DOTAAbilityTooltip").FindChildTraverse('AbilityExtraAttributes')
+					var parent_container = container.GetParent()					
+					var extra_attributes = DotaHud.FindChildTraverse("DOTAAbilityTooltip").FindChildTraverse('AbilityCoreDetails').FindChildTraverse("imba_invoker_tooltip")
+
+					if (extra_attributes) {
+						extra_attributes.text = new_ability_text
+						extra_attributes.style.visibility = "visible";
+						container.style.visibility = "collapse";
+					} else {
+/*
+						extra_attributes = $.CreatePanel('Label', parent_container, 'imba_invoker_tooltip')
+//						extra_attributes.style['letter-spacing'] = '0px';
+						extra_attributes.style['padding-left'] = '8px';
+						extra_attributes.style['color'] = '#596e89';
+						extra_attributes.style['font-size'] = '16px';
+						extra_attributes.style['font-weight'] = 'normal';
+						extra_attributes.style['font-family'] = 'Radiance';
+						extra_attributes.html = true;
+						extra_attributes.AddClass('imba_invoker_tooltip')
+						extra_attributes.text = new_ability_text
+						parent_container.MoveChildBefore(extra_attributes, container);
+						container.style.visibility = "collapse";
+*/
+					}
+				}
+
+				AbilityCooldown
 			}
-		)
-
+		}
+	} else {
+		print("Tried to invoke invoker tooltip function wrong hero-class")
 	}
+}
 
-	// Toggle Visibility for Invokers invoke spell list
-	if (DotaHud.FindChildTraverse("Ability5") && DotaHud.FindChildTraverse("Ability5").Children() && DotaHud.FindChildTraverse("Ability5").Children()[1])
-		DotaHud.FindChildTraverse("Ability5").Children()[1].style.visibility = "visible"
+function ToggleHeroTooltips() {
+	var extra_attributes = DotaHud.FindChildTraverse("DOTAAbilityTooltip").FindChildTraverse('AbilityCoreDetails').FindChildTraverse("imba_invoker_tooltip")
 
-	// Remove onmouseover tooltips from Inokver's Spell list... they bug out completely 
-	var spell_card_icons = DotaHud.FindChildTraverse("lower_hud").FindChildTraverse("SpellRowContainer").FindChildrenWithClassTraverse("AbilityMaxLevel")
-	if (spell_card_icons != null && spell_card_icons.length == 10)
-	{
-		for(var i = 0; i < 10; i++) 
-		{
-			var ability = spell_card_icons[i];
-			var ability_name = spell_card_icons[i].abilityname;
+	if(extra_attributes) {
+		extra_attributes.style.visibility = "collapse";
+//		DotaHud.FindChildTraverse("DOTAAbilityTooltip").FindChildTraverse('AbilityCoreDetails').GetChild(7).style.visibility = "visible"
+	}
+}
 
-			(function (ability, ability_name) {
-				ability.SetPanelEvent("onmouseover", function () {
-					$.DispatchEvent("DOTAShowAbilityTooltip", ability, "imba_" + ability_name);
+function init_tooltips(keys) {
+	$.Msg(keys)
+
+	for (var i = 0; i <= 5; i++) {
+		var ability = DotaHud.FindChildTraverse("Ability" + i);
+
+		if (ability) {
+			var ability_name = ability.FindChildTraverse("AbilityImage").abilityname.substring(5);
+			var tooltip_checker_string = "DOTA_Tooltip_Ability_" + ability_name;
+			var tooltip_checker = $.Localize(tooltip_checker_string);
+
+
+			print(tooltip_checker)
+
+			if (tooltip_checker == tooltip_checker_string) {
+				print("missing vanilla tooltip for ability " + ability_name + "! defaults to imba tooltips.")
+				ability_name = ability.FindChildTraverse("AbilityImage").abilityname;
+			}
+
+			(function (ability, ability_name, i) {
+				ability.SetPanelEvent("onmouseover", function() {
+					$.DispatchEvent("DOTAHideAbilityTooltip", ability); // Disable vanilla tooltips
+					$.DispatchEvent("DOTAShowAbilityTooltip", ability, ability_name); // Disable vanilla tooltips
+//					AddHeroTooltips(i);
 				})
-				ability.SetPanelEvent("onmouseout", function () {
-					$.DispatchEvent("DOTAHideAbilityTooltip", ability);
-				})
-			})(ability, ability_name);
+
+//				ability.SetPanelEvent("onmouseout", function() {
+//					ToggleHeroTooltips();
+//				})
+			})(ability, ability_name, i);
 		}
 	}
 }
 
-GameEvents.Subscribe("invoker_helper", init_invoker_tooltips);
+for (var i = 0; i <= 19; i++) {
+	GameEvents.Subscribe("vanilla_abilities_" + i, init_tooltips);
+}
 
 //print(DotaHud.FindChildTraverse("DOTAAbilityTooltip").FindChildTraverse("AbilityName").text)
 //print(DotaHud.FindChildTraverse("DOTAAbilityTooltip").FindChildTraverse('AbilityExtraAttributes'))
--- game/dota_addons/dota_imba_reborn/scripts/vscripts/components/vanilla_link/init.lua
+++ game/dota_addons/dota_imba_reborn/scripts/vscripts/components/vanilla_link/init.lua
@@ -1,7 +1,7 @@
 ListenToGameEvent('npc_spawned', function(keys)
 	local npc = EntIndexToHScript(keys.entindex)
 
-	if npc and not npc.first_spawn and npc:IsRealHero() then
+	if npc and not npc.first_spawn and npc:IsRealHero() and npc == PlayerResource:GetPlayer(npc:GetPlayerID()):GetAssignedHero() then
 		npc.first_spawn = true
 
 		local abilities = {}
@@ -10,10 +10,19 @@ ListenToGameEvent('npc_spawned', function(keys)
 			local ability = npc:GetAbilityByIndex(i)
 
 			if ability then
-				table.insert(abilities, i, string.gsub(ability:GetAbilityName(), "imba_", ""))
+				local vanilla_ability_name = string.gsub(ability:GetAbilityName(), "imba_", "")
+				table.insert(abilities, i, vanilla_ability_name)
+				npc:AddAbility(vanilla_ability_name)
+				local vanilla_ability = npc:FindAbilityByName(vanilla_ability_name)
+				if vanilla_ability then
+					vanilla_ability:SetHidden(true)
+				else
+					print("Vanilla ability not found:", vanilla_ability_name)
+					return
+				end
 			end
 		end
 
-		CustomGameEventManager:Send_ServerToAllClients("vanilla_abilities_"..npc:entindex(), abilities)
+		CustomGameEventManager:Send_ServerToAllClients("vanilla_abilities_"..npc:GetPlayerID(), abilities)
 	end
 end, nil)
