8e47867f55b403483b7920e4bb9385c11eb5ee82
EarthSalamander <jeizyy30@hotmail.fr>
2021-02-16 16:13:20 +0100

    Banned Treant and Undying + games not starting fix

--- game/dota_addons/dota_imba_reborn/scripts/vscripts/components/new_team_selection.lua
+++ game/dota_addons/dota_imba_reborn/scripts/vscripts/components/new_team_selection.lua
@@ -16,15 +16,27 @@ end
 -- This function is ONLY for testing purposes
 ListenToGameEvent('game_rules_state_change', function()
 	if GameRules:State_Get() == DOTA_GAMERULES_STATE_CUSTOM_GAME_SETUP then
-		if (GetMapName() == "imba_5v5" or GetMapName() == "imba_10v10") and IsInToolsMode() then
---		if GetMapName() == "imba_5v5" and IsInToolsMode() then
-			GameRules:GetGameModeEntity():SetContextThink(DoUniqueString("anti_stacks_fucker"), function()
-				-- This function is called when connection to backend is successful if not in tools mode, let's call it in tools mode when bots are added in for testing purpose
-				TeamOrdering:ComputeTeamSelection()
+--		if IsInToolsMode() then
+--			if (GetMapName() == "imba_5v5" or GetMapName() == "imba_10v10") then
+--				GameRules:GetGameModeEntity():SetContextThink(DoUniqueString("anti_stacks_fucker"), function()
+					-- This function is called when connection to backend is successful if not in tools mode, let's call it in tools mode when bots are added in for testing purpose
+--					TeamOrdering:ComputeTeamSelection()
 
-				return nil
-			end, 3.0)
-		end
+--					return nil
+--				end, 3.0)
+--			end
+--		else
+			-- only if BP is disabled, otherwise call it in battle pass scripts
+			if not Battlepass then
+				if CUSTOM_GAME_TYPE == "IMBA" then
+					print("BP disabled, hello there")
+					TeamOrdering:OnPlayersLoaded()
+				end
+			end
+--		end
+
+		-- ultimate fail-safe in case something goes wrong
+		GameRules:SetCustomGameSetupRemainingTime(20.0)
 	elseif GameRules:State_Get() == DOTA_GAMERULES_STATE_HERO_SELECTION then
 		-- Call it here to re-apply players to rightful teams in case a smart boi use shuffle command as lobby leader
 
